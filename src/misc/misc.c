7528669c117f3fcda7d0bfd069e6c790f0605e71
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2018-05-01 11:10:46 +0200

    missing math header

--- src/misc/misc.c
+++ src/misc/misc.c
@@ -1,11 +1,11 @@
 /* Copyright 2013-2015. The Regents of the University of California.
- * Copyright 2015. Martin Uecker.
+ * Copyright 2015-2018. Martin Uecker.
  * Copyright 2017. University of Oxford.
  * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  *
  * Authors:
- * 2011-2015 Martin Uecker <martin.uecker@med.uni-goettingen.de>
+ * 2011-2018 Martin Uecker <martin.uecker@med.uni-goettingen.de>
  * 2017 Sofia Dimoudi <sofia.dimoudi@cardiov.ox.ac.uk>
  */
 
