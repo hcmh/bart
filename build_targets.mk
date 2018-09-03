1a62097e830e5eeb874243bebec088fdd9afa1a9
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2018-09-03 11:35:39 +0200

    rotations using ffts

--- build_targets.mk
+++ build_targets.mk
@@ -2,7 +2,7 @@
 #
 TBASE=show slice crop resize join transpose squeeze flatten zeros ones flip circshift extract repmat bitmask reshape version delta copy casorati vec poly
 TFLP=scale invert conj fmac saxpy sdot spow cpyphs creal carg normalize cdf97 pattern nrmse mip avg cabs zexpj
-TNUM=fft fftmod fftshift noise bench threshold conv rss filter mandelbrot wavelet window var std
+TNUM=fft fftmod fftshift noise bench threshold conv rss filter mandelbrot wavelet window var std fftrot
 TRECO=pics pocsense sqpics itsense nlinv T1fun nufft rof sake wave lrmatrix estdims estshift estdelay wavepsf wshfl hornschunck ncsense kmat power approx kernel dcnn
 TCALIB=ecalib ecaltwo caldir walsh cc ccapply calmat svd estvar whiten
 TMRI=homodyne poisson twixread fakeksp umgread
