79f666eab334fca42c2093b04c76b3056b90b2e0
rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2018-11-07 15:56:54 +0100

    rtnlinv: SMS slice ordering

--- src/rtnlinv.c
+++ src/rtnlinv.c
@@ -122,7 +122,7 @@ int main_rtnlinv(int argc, char* argv[])
 	if (1 != k_s->dims_full[SLICE_DIM]) {
 
 		debug_printf(DP_INFO, "SMS-NLINV reconstruction. Multiband factor: %d\n", k_s->dims_full[SLICE_DIM]);
-		//fftmod(DIMS, k_s->dims_full, SLICE_FLAG, k, k); // fftmod to get correct slice order in output
+		fftmod(DIMS, k_s->dims_full, SLICE_FLAG, k, k); // fftmod to get correct slice order in output
 	}
 
 	complex float* pattern = NULL;
