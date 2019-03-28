20f977634fb306126d538df57b27cbb15dafc1b5
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2019-03-28 17:04:34 +0100

    test reading of mpi files

--- tests/twixread.mk
+++ tests/twixread.mk
@@ -1,14 +1,21 @@
 
-TWIXREAD=/home/ague/archive/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
+TWIXFILE1=/home/ague/archive/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
+TWIXFILE2=/home/ague/data/T1825/T1825.dat
 
-tests/test-twixread: twixread ${TWIXREAD}
+tests/test-twixread: twixread ${TWIXFILE1}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/twixread -A ${TWIXREAD} ksp.ra					;\
-	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" > ksp.md5sum			;\
-	md5sum -c ksp.md5sum								;\
-	rm ksp.md5sum ksp.ra ; cd .. ; rmdir $(TESTS_TMP)
+	$(TOOLDIR)/twixread -A ${TWIXFILE1} ksp.ra					;\
+	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" | md5sum -c			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+tests/test-twixread-mpi: twixread ${TWIXFILE2}
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/twixread -M -x320 -r11 -s3 -n217 -c34 ${TWIXFILE2} ksp.ra		;\
+	echo "9ee480b68ed70a7388851277e209b38d *ksp.ra" | md5sum -c			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-TESTS_AGUE += tests/test-twixread
+TESTS_AGUE += tests/test-twixread tests/test-twixread-mpi
 
