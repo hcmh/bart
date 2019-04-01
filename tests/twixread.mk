c03043a82d7c8c91cf65a7e70d0082bc5a800478
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2019-04-01 11:59:35 +0200

    multi-raid test

--- tests/twixread.mk
+++ tests/twixread.mk
@@ -2,8 +2,16 @@
 TWIXFILE1=/home/ague/archive/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
 TWIXFILE2=/home/ague/data/T1825/T1825.dat
 TWIXFILE3=/home/ague/archive/2018/2018-04-13_RT-Export/U00000013.dat
+TWIXFILE4=/home/ague/archive/2019/2019-03-25_Other-MU_Export/meas_MID00311_FID65562_t1_fl2d.dat
 
 tests/test-twixread: twixread ${TWIXFILE1}
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/twixread -A ${TWIXFILE1} ksp.ra					;\
+	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" | md5sum -c			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+tests/test-twixread-multiraid: twixread ${TWIXFILE4}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/twixread -A ${TWIXFILE1} ksp.ra					;\
 	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" | md5sum -c			;\
@@ -26,6 +34,5 @@ tests/test-umgread: umgread ${TWIXFILE3}
 
 
 
-
-TESTS_AGUE += tests/test-twixread tests/test-twixread-mpi tests/test-umgread
+TESTS_AGUE += tests/test-twixread tests/test-twixread-multiraid tests/test-twixread-mpi tests/test-umgread
 
