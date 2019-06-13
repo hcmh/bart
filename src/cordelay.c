07b67c25d9045028e7c4e6bca1e3b4f4c32b2d95
rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2019-06-13 14:05:42 +0200

    cordelay: Correct gradient delays on data

---
+++ src/cordelay.c
@@ -0,0 +1,138 @@
+/* Copyright 2017. The Regents of the University of California.
+ * Copyright 2017. Sebastian Rosenzweig.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
+ *
+ * Authors:
+ * 2017 Sebastian Rosenzweig
+ */
+
+#include <stdbool.h>
+#include <complex.h>
+#include <math.h>
+
+#include "num/multind.h"
+#include "num/init.h"
+#include "num/fft.h"
+#include "num/flpmath.h"
+
+
+#include "misc/mmio.h"
+#include "misc/misc.h"
+#include "misc/mri.h"
+#include "misc/opts.h"
+
+
+
+
+
+
+static const char usage_str[] = "<kspace> <traj> <output> ";
+static const char help_str[] = "Gradient Delay correction on data\n";
+
+int main_cordelay(int argc, char* argv[])
+{
+	float gdelays[3] = { 0., 0., 0. };
+
+	const struct opt_s opts[] = {
+		OPT_FLVEC3('q', &gdelays, "delays", "gradient delays: x, y, xy"),
+	};
+
+	cmdline(&argc, argv, 3, 3, usage_str, help_str, ARRAY_SIZE(opts), opts);
+
+
+	num_init();
+
+	long k_dims[DIMS];
+	complex float* k = load_cfl(argv[1], DIMS, k_dims);
+
+	long traj_dims[DIMS];
+	complex float* traj = load_cfl(argv[2], DIMS, traj_dims);
+
+	md_check_compat(DIMS, ~(READ_FLAG|COIL_FLAG), k_dims, traj_dims);
+
+
+	// Choose only a single RO sample for angle calculation
+	long traj_red_dims[DIMS];
+	md_select_dims(DIMS, ~PHS1_FLAG, traj_red_dims, traj_dims);
+
+	complex float* traj_red = md_alloc(DIMS, traj_red_dims, CFL_SIZE);
+
+	long pos[DIMS] = {0};
+	md_slice(DIMS, PHS1_FLAG, pos, traj_dims, traj_red, traj, CFL_SIZE);
+
+
+	long angles_dims[DIMS];
+	md_select_dims(DIMS, PHS2_FLAG|TIME_FLAG|SLICE_FLAG, angles_dims, k_dims);
+	complex float* angles = md_alloc(DIMS, angles_dims, CFL_SIZE);
+
+	long dk_dims[DIMS];
+	md_copy_dims(DIMS, dk_dims, angles_dims);
+	complex float* dk = md_alloc(DIMS, dk_dims, CFL_SIZE);
+
+ 	long dk_strs[DIMS];
+	md_calc_strides(DIMS, dk_strs, dk_dims, CFL_SIZE);
+
+	// Calculate projection 'angles' and corresponding shifts 'dk'
+	unsigned long N = k_dims[PHS2_DIM] * k_dims[TIME_DIM] * k_dims[SLICE_DIM];
+
+	for (unsigned long i = 0; i < N; i++) {
+
+		angles[i] = (complex float)(M_PI + atan2f(crealf(traj_red[3 * i + 0]), crealf(traj_red[3 * i + 1])));
+
+		dk[i] = (complex float)(gdelays[0] * pow(cosf(angles[i]),2) + gdelays[1] * pow(sinf(angles[i]),2) + 2 *  gdelays[2] * sinf(angles[i]) * cosf(angles[i]));
+	}
+
+
+
+	// Create phase-ramps
+	unsigned long RO = k_dims[PHS1_DIM];
+
+	complex float* ramps = md_alloc(DIMS, k_dims, CFL_SIZE);
+	long ramps_strs[DIMS];
+	md_calc_strides(DIMS, ramps_strs, k_dims, CFL_SIZE);
+
+
+	long ramps_singleton_dims[DIMS];
+	md_select_dims(DIMS, PHS1_FLAG, ramps_singleton_dims, k_dims);
+
+	long ramps_singleton_strs[DIMS];
+	md_calc_strides(DIMS, ramps_singleton_strs, ramps_singleton_dims, CFL_SIZE);
+
+	complex float* ramps_singleton = md_alloc(DIMS, ramps_singleton_dims, CFL_SIZE);
+
+	for (unsigned long i = 0; i < RO; i++)
+		ramps_singleton[i] = -(complex float) RO / 2. + i;
+
+
+
+
+	md_copy2(DIMS, k_dims, ramps_strs, ramps, ramps_singleton_strs, ramps_singleton, CFL_SIZE);
+	md_zmul2(DIMS, k_dims, ramps_strs, ramps, ramps_strs, ramps, dk_strs, dk);
+	md_zsmul(DIMS, k_dims, ramps, ramps, 2. * M_PI / RO);
+	md_zexpj(DIMS, k_dims, ramps, ramps);
+
+
+
+
+	// Shift k-space samples using Fourier-Shift-Theorem
+	complex float* k_cor = create_cfl(argv[3], DIMS, k_dims);
+	complex float* k_trans = md_alloc(DIMS, k_dims, CFL_SIZE);
+
+	ifftuc(DIMS, k_dims, PHS1_FLAG, k_trans, k);
+	md_zmul(DIMS, k_dims, k_trans, k_trans, ramps);
+	fftuc(DIMS, k_dims, PHS1_FLAG, k_cor, k_trans);
+
+	md_free(traj_red);
+	md_free(angles);
+	md_free(dk);
+	md_free(ramps_singleton);
+	md_free(ramps);
+	md_free(k_trans);
+
+	unmap_cfl(DIMS, k_dims, k_cor);
+	unmap_cfl(DIMS, traj_dims, traj);
+	unmap_cfl(DIMS, k_dims, k);
+
+	exit(0);
+}
