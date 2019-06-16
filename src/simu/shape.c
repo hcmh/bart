6529f25e35d9aa8620431b2eb43e1cefb1ae4f56
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2019-06-16 13:54:05 +0200

    kpolygon, support off-center shapes

--- src/simu/shape.c
+++ src/simu/shape.c
@@ -16,8 +16,6 @@ complex double xpolygon(int N, const double pg[N][2], const double p[3])
 	return (0 == w % 2) ? 0. : 1.;
 }
 
-
-
 static double sinc(double x)
 {
 	return (0. == x) ? 1. : (sin(x) / x);
@@ -32,11 +30,9 @@ static double sdot(const double a[2], const double b[2])
 // https://arxiv.org/pdf/1703.00255.pdf
 // FIXME: series expansion cases are missing
 
-complex double kpolygon(int N, const double pg[N][2], const double q0[3])
+static complex double kpolygon1(int N, const double pg[N][2], const double c[2], const double q0[2])
 {
-	assert(1. == polygon_winding_number(N, pg, (double[]){ 0., 0. }));
-
-	assert(0. == q0[2]);
+	assert(1. == polygon_winding_number(N, pg, c));
 
 	double q[2];
 
@@ -58,6 +54,9 @@ complex double kpolygon(int N, const double pg[N][2], const double q0[3])
 		double E[2] = { pg[i][0] - pg[(i - 1 + N) % N][0],  pg[i][1] - pg[(i - 1 + N) % N][1] };
 		double R[2] = { pg[i][0] + pg[(i - 1 + N) % N][0],  pg[i][1] + pg[(i - 1 + N) % N][1] };
 
+		R[0] -= 2. * c[0];
+		R[1] -= 2. * c[1];
+
 		complex double x = sinc(sdot(q, E)) * cexp(1.i * sdot(q, R));
 
 		sum[0] += E[0] * x;
@@ -65,5 +64,32 @@ complex double kpolygon(int N, const double pg[N][2], const double q0[3])
 	}
 		
 	return (qx[0] * sum[0] + qx[1] * sum[1]) / (8.i * q2);
+}
+
+complex double kpolygon(int N, const double pg0[N][2], const double q0[3])
+{
+	double pg[N][2];
+
+	for (int i = 0; i < N; i++) {
+
+		pg[i][0] = pg0[i][0];
+		pg[i][1] = pg0[i][1];
+	}
+
+	double cg[2] = { 0., 0. };
+
+	for (int i = 0; i < N; i++) {
+
+		cg[0] += pg[i][0];
+		cg[1] += pg[i][1];
+	}
+
+	cg[0] /= N;
+	cg[1] /= N;
+
+	assert(0. == q0[2]);
+
+	return cexp(M_PI * 2.i * sdot(cg, q0)) * kpolygon1(N, pg, cg, q0);
+//	return kpolygon1(N, pg, (double[]){ 0., 0. }, q0);
 }
 
