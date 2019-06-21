9d0b846c073b889dc6bb5c8d8d16827c864e3aca
rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2019-06-21 13:31:17 +0200

    tests: generalized laplace

--- tests/laplace.mk
+++ tests/laplace.mk
@@ -17,4 +17,20 @@ tests/test-laplace: ones zeros join laplace flip scale delta saxpy nrmse
 	touch $@
 
 
-TESTS += tests/test-laplace
+tests/test-laplace-gen: ones zeros join laplace flip scale delta saxpy nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+		$(TOOLDIR)/ones 2 10 10 o.ra				;\
+		$(TOOLDIR)/zeros 2 10 10 z.ra				;\
+		$(TOOLDIR)/join 0  o.ra z.ra o.ra z.ra o.ra z.ra j.ra	;\
+		$(TOOLDIR)/laplace -n1 -s1 -g j.ra Lg.ra			;\
+		$(TOOLDIR)/flip 1 j.ra jf.ra				;\
+		$(TOOLDIR)/join 1 j.ra jf.ra j.ra jf.ra j.ra jf.ra T.ra	;\
+		$(TOOLDIR)/scale -- 3.333e-2 T.ra T1.ra			;\
+		$(TOOLDIR)/nrmse -t 0.002 T1.ra Lg.ra			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+	
+	
+
+
+TESTS += tests/test-laplace tests/test-laplace-gen
