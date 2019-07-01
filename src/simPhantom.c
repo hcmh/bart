53b902171b55fd1f4ecf23a73f623946289d3479
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2019-07-01 20:13:27 +0200

    QuantMap:: Update simulation tool.
    Additionals:
    	- multiple spins
    	- cauchy distribution of off-resonance frequencies within voxel
    	- linear offset profile for phantoms
    	- single voxel option
    	- external off-resonace, flipangle and rf-duration access
    	-

--- src/simPhantom.c
+++ src/simPhantom.c
@@ -31,9 +31,9 @@ int main_simPhantom(int argc, char* argv[argc])
 {   
 	
 	//Measure time elapse
-    struct timeval t1, t2;
+    struct timeval t_start, t_end;
     double elapsedTime;
-    gettimeofday(&t1, NULL);
+    gettimeofday(&t_start, NULL);
 	
 	
 	
@@ -43,124 +43,163 @@ int main_simPhantom(int argc, char* argv[argc])
 	int repetition = 500;
     int kspace = 0;
 	int aver_num = 1;
+	int spin_num = 1;
 	bool analytical = false;
+	bool spin_ensamble = false;
 	float tr = 0.0045;
 	float te = 0.00225;
+	float flipangle = 45.;
+	float rf_end = 0.0009;
+	float offresonance = 0.;
 	bool inverse_relaxation = false;
 	const char* inputRel1 = NULL;
 	const char* inputRel2 = NULL;
 	const char* inputM0 = NULL;
 	
+	float t1i = WATER_T1;
+	float t2i = WATER_T2;
+	float m0i = 1;
+	
+	bool linear_offset = false;
+	
     const struct opt_s opts[] = {
-        OPT_INT('s', &seq, "sequence", "options: 0 = bSSFP[default], 1 = invbSSFP, 2 = FLASH, 3 = pcbSSFP, 4 = turboSE, 5 = invFLASH, 6 = invpcbSSFP"),
+		
+		/* Sequence Info */
+        OPT_INT('s', &seq, "sequence", "options: 0 = bSSFP[default], 1 = invbSSFP, 3 = pcbSSFP, 4 = turboSE, 5 = invFLASH, 6 = invpcbSSFP"),
         OPT_INT('x', &xdim, "n", "dimensions in x for shepp-logan phantom"),
         OPT_INT('y', &ydim, "n", "dimensions in y for shepp-logan phantom"),
-        OPT_INT('r', &repetition, "n", "repetitions/train-length"),
-        OPT_INT('a', &aver_num, "n", "number of averaged TRs"),
-        OPT_INT('d', &debug_level, "level", "Debug level"),        
-        OPT_INT('k', &kspace, "d", "kspace output? default:0=no"),
-        OPT_SET('A', &analytical, "Use analytical model for simulation"),
         OPT_FLOAT('t', &tr, "", "TR [s]"),
 		OPT_FLOAT('e', &te, "", "TE [s]"),
-		/*Input Maps */
+		OPT_FLOAT('f', &flipangle, "", "Flip angle [deg]"),
+		OPT_FLOAT('p', &rf_end, "", "RF pulse duration [s]"),
+        
+        /* Voxel Info */
+		OPT_INT('a', &aver_num, "n", "number of averaged TRs"),
+		OPT_INT('n', &spin_num, "n", "number of spins"),
+        OPT_INT('r', &repetition, "n", "repetitions/train-length"),
+        OPT_FLOAT('w', &offresonance, "", "off-resonance frequency [rad]"),
+        
+		/* Input Maps */
 		OPT_SET('R', &inverse_relaxation, "Input inverse relaxation?"),
 		OPT_STRING('I', &inputRel1, "Input Rel1", "Input relaxation parameter 1."),
 		OPT_STRING('i', &inputRel2, "Input Rel2", "Input relaxation parameter 2."),
 		OPT_STRING('M', &inputM0, "Input M0", "Input M0."),
+		
+		/*for x == 1 && y == 1 */
+		OPT_FLOAT('m', &m0i, "M0 [s]", "for x & y == 1"),
+		OPT_FLOAT('1', &t1i, "T1 [s]", "for x & y == 1"),
+		OPT_FLOAT('2', &t2i, "T2 [s]", "for x & y == 1"),
+		
+		/* Special Cases */
+		OPT_SET('A', &analytical, "Use analytical model for simulation"),
+		OPT_INT('d', &debug_level, "level", "Debug level"),   
+		OPT_SET('E', &spin_ensamble, "Input inverse relaxation?"),
+		OPT_INT('k', &kspace, "d", "kspace output? default:0=no"),
+		OPT_SET('L', &linear_offset, "Add linear distribution of off-set freq."),
     };
     
     cmdline(&argc, argv, 4, 4, usage_str, help_str, ARRAY_SIZE(opts), opts);
     num_init();
     
-    //------------------------------------------------------------
-    //------------ Parameter for simulation  --------------------
-    //------------------------------------------------------------
-	float angle;
-    if(seq == 2 || seq == 5) 	//Flash or inv. prep. Flash
-		angle = 4.;
-	else			//bSSFP,...
-		angle = 45.;
-    
-    //------------------------------------------------------------
-    //--------------  Create simulation dataset  -----------------
-    //------------------------------------------------------------
+	/* --------------------------------------------------------------
+     * ---------------  Starting Simulation  ------------------------
+     * --------------------------------------------------------------
+		Sequences:
+		(seq){
+			case 0: 	normal bSSFP
+			case 1: 	inversion recovery prepared bSSFP
+			case 2:		FLASH
+			case 3: 	pcbSSFP
+			case 5: 	inversion recovery prepared FLASH
+			case 6: 	inversion recovery prepared pcbSSFP
+			
+		}
+	* --------------------------------------------------------------
+	*/
 	long dim_map[DIMS] = { [0 ... DIMS - 1] = 1 };
-	
-	
-	
+
 	complex float* map_T1;
 	complex float* map_T2;
 	complex float* map_M0;
 	
-	// Create Shepp-Logan phantom if no maps are given
-	if ( (NULL == inputRel1) && (NULL == inputRel2) && (NULL == inputM0) )
+	if (xdim != 1 && ydim != 1)
 	{
+		// Create Shepp-Logan phantom if no maps are given
+		if ( (NULL == inputRel1) && (NULL == inputRel2) && (NULL == inputM0) )
+		{
+			dim_map[0] = xdim;
+			dim_map[1] = ydim;
+			
+			map_T1 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			map_T2 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			map_M0 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			
+			complex float* samples = NULL;
+			long sstrs[DIMS];		
+			
+			complex float* map = create_cfl("T1T2Parameter", DIMS, dim_map);
+			md_clear(DIMS, dim_map, map, CFL_SIZE);
+			calc_phantom_t1t2(dim_map, map, false/*d3*/, false/*kspace*/, sstrs, samples);
+			
+			complex float* dens = create_cfl("DensParameter", DIMS, dim_map);
+			md_clear(DIMS, dim_map, dens, CFL_SIZE);
+			calc_phantom_dens(dim_map, dens, false/*d3*/, false/*kspace*/, sstrs, samples);
+			
+			md_zreal(DIMS, dim_map, map_T1, map);
+			md_zimag(DIMS, dim_map, map_T2, map);
+			md_zabs(DIMS, dim_map, map_M0, dens);
+			
+			unmap_cfl(DIMS, dim_map, map);
+			unmap_cfl(DIMS, dim_map, dens);
+			
+			
+			
+		} else { // Create Phantom based on given Relaxation and M0 Maps
+			
+			complex float* input_map_T1 = NULL;
+			if ( NULL != inputRel1 ) 
+				input_map_T1 = load_cfl(inputRel1, DIMS, dim_map);
+			else
+				debug_printf(DP_WARN, "No input for relaxation parameter 1 could be found!");
+				
+			complex float* input_map_T2 = NULL;
+			if ( NULL != inputRel2 ) 
+				input_map_T2 = load_cfl(inputRel2, DIMS, dim_map);
+			else
+				debug_printf(DP_WARN, "No input for relaxation parameter 2 could be found!");
+				
+			complex float* input_map_M0 = NULL;
+			if ( NULL != inputM0 ) 
+				input_map_M0 = load_cfl(inputM0, DIMS, dim_map);
+			else
+				debug_printf(DP_WARN, "No input for M0 could be found!");
+			
+			
+			map_T1 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			map_T2 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			map_M0 = md_alloc(DIMS, dim_map, CFL_SIZE);
+			
+			md_copy(DIMS, dim_map, map_T1, input_map_T1, CFL_SIZE);
+			md_copy(DIMS, dim_map, map_T2, input_map_T2, CFL_SIZE);
+			md_copy(DIMS, dim_map, map_M0, input_map_M0, CFL_SIZE);
+			
+			xdim = dim_map[0];
+			ydim = dim_map[1];
+			
+			if( NULL != inputRel1 )
+				unmap_cfl(DIMS, dim_map, input_map_T1);
+			
+			if( NULL != inputRel2 )
+				unmap_cfl(DIMS, dim_map, input_map_T2);
+			
+			if( NULL != inputM0 )
+				unmap_cfl(DIMS, dim_map, input_map_M0);
+		}
+	} else {
 		dim_map[0] = xdim;
 		dim_map[1] = ydim;
-		
-		map_T1 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		map_T2 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		map_M0 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		
-		complex float* samples = NULL;
-		long sstrs[DIMS];		
-		
-		complex float* map = create_cfl("T1T2Parameter", DIMS, dim_map);
-		md_clear(DIMS, dim_map, map, CFL_SIZE);
-		calc_phantom_t1t2(dim_map, map, false/*d3*/, false/*kspace*/, sstrs, samples);
-		
-		complex float* dens = create_cfl("DensParameter", DIMS, dim_map);
-		md_clear(DIMS, dim_map, dens, CFL_SIZE);
-		calc_phantom_dens(dim_map, dens, false/*d3*/, false/*kspace*/, sstrs, samples);
-		
-		md_zreal(DIMS, dim_map, map_T1, map);
-		md_zimag(DIMS, dim_map, map_T2, map);
-		md_zabs(DIMS, dim_map, map_M0, dens);
-		
-		unmap_cfl(DIMS, dim_map, map);
-		unmap_cfl(DIMS, dim_map, dens);
-		
-		
-		
-	} else { // Create Phantom based on given Relaxation and M0 Maps
-		
-		complex float* input_map_T1 = NULL;
-		if ( NULL != inputRel1 ) 
-			input_map_T1 = load_cfl(inputRel1, DIMS, dim_map);
-		else
-			debug_printf(DP_WARN, "No input for relaxation parameter 1 could be found!");
-			
-		complex float* input_map_T2 = NULL;
-		if ( NULL != inputRel2 ) 
-			input_map_T2 = load_cfl(inputRel2, DIMS, dim_map);
-		else
-			debug_printf(DP_WARN, "No input for relaxation parameter 2 could be found!");
-			
-		complex float* input_map_M0 = NULL;
-		if ( NULL != inputM0 ) 
-			input_map_M0 = load_cfl(inputM0, DIMS, dim_map);
-		else
-			debug_printf(DP_WARN, "No input for M0 could be found!");
-		
-		
-		map_T1 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		map_T2 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		map_M0 = md_alloc(DIMS, dim_map, CFL_SIZE);
-		
-		md_copy(DIMS, dim_map, map_T1, input_map_T1, CFL_SIZE);
-		md_copy(DIMS, dim_map, map_T2, input_map_T2, CFL_SIZE);
-		md_copy(DIMS, dim_map, map_M0, input_map_M0, CFL_SIZE);
-		
-		if( NULL != inputRel1 )
-			unmap_cfl(DIMS, dim_map, input_map_T1);
-		
-		if( NULL != inputRel2 )
-			unmap_cfl(DIMS, dim_map, input_map_T2);
-		
-		if( NULL != inputM0 )
-			unmap_cfl(DIMS, dim_map, input_map_M0);
 	}
-     
+	
     //------------------------------------------------------------
     //------------  Simulation of phantom data  ------------------
     //------------------------------------------------------------
@@ -181,19 +220,20 @@ int main_simPhantom(int argc, char* argv[argc])
     for(int x = 0; x < dim_phantom[0]; x++ ){
         
         for(int y = 0; y < dim_phantom[1]; y++ ){
-            
-			printf("Pixel (x, y):\t(%d, %d)\n", x, y);
-			
-            
-            //Get offset values from -pi to +pi
-            //data.w = ( (float) y - (float) dim_phantom[1]/2. )/  ( (float) dim_phantom[1]/2.) * PI / data.TR;
-            
+
             //Updating data
-            float t1 = cabs(map_T1[(y * dim_phantom[0]) + x]);  
-            float t2 = cimagf(map_T2[(y * dim_phantom[0]) + x]); 
-			float m0 = cabsf(map_M0[(y * dim_phantom[0]) + x]);
-            
-			
+			float t1; float t2; float m0;
+			if (xdim != 1 && ydim != 1)
+			{
+				t1 = cabs(map_T1[(y * dim_phantom[0]) + x]);  
+				t2 = cimagf(map_T2[(y * dim_phantom[0]) + x]); 
+				m0 = cabsf(map_M0[(y * dim_phantom[0]) + x]);
+			} else {
+				t1 = t1i;
+				t2 = t2i;
+				m0 = m0i;
+			}
+
             //Skip empty voxels
             if(t1 <= 0.001 || t2 <= 0.001){
                 for(int z = 0; z < dim_phantom[TE_DIM]; z++){
@@ -205,7 +245,7 @@ int main_simPhantom(int argc, char* argv[argc])
                 continue;
             }
             
-			
+
 			struct SimData sim_data;
 	
 			sim_data.seqData = seqData_defaults;
@@ -213,23 +253,28 @@ int main_simPhantom(int argc, char* argv[argc])
 			sim_data.seqData.TR = tr;
 			sim_data.seqData.TE = te;
 			sim_data.seqData.rep_num = repetition;
-			sim_data.seqData.spin_num = 1;
+			sim_data.seqData.spin_num = spin_num;
 			sim_data.seqData.num_average_rep = aver_num;
 			
 			sim_data.voxelData = voxelData_defaults;
 			sim_data.voxelData.r1 = 1 / t1;
 			sim_data.voxelData.r2 = 1 / t2;
 			sim_data.voxelData.m0 = m0;
-			sim_data.voxelData.w = 0;
+			sim_data.voxelData.spin_ensamble = spin_ensamble;
+			
+			if ( linear_offset )
+				//Get offset values from -pi to +pi
+				sim_data.voxelData.w = (float) y / (float) dim_phantom[1]  * PI / sim_data.seqData.TE; 
+			else
+				sim_data.voxelData.w = offresonance;
+			
+			
 			
 			sim_data.pulseData = pulseData_defaults;
-			sim_data.pulseData.flipangle = angle;
-			sim_data.pulseData.RF_end = 0.0009;
+			sim_data.pulseData.flipangle = flipangle;
+			sim_data.pulseData.RF_end = rf_end;
 			sim_data.gradData = gradData_defaults;
 			sim_data.seqtmp = seqTmpData_defaults;
-			
-// 			printf("%f,\t%f,\t%f,\t%f,\t%d,\t%f,\t%f,\n", sim_data.seqData.TR, sim_data.seqData.TE, sim_data.voxelData.r1,sim_data.voxelData.r2,sim_data.seqtmp.rep_counter, sim_data.pulseData.RF_end, sim_data.pulseData.flipangle );
-			
 			
 			float mxySig[sim_data.seqData.rep_num / sim_data.seqData.num_average_rep][3];
 			float saR1Sig[sim_data.seqData.rep_num / sim_data.seqData.num_average_rep][3];
@@ -238,7 +283,7 @@ int main_simPhantom(int argc, char* argv[argc])
 
 			
 			
-			float fa = angle * M_PI / 180.; //conversion to rad
+			float fa = flipangle * M_PI / 180.; //conversion to rad
 			
 			if(analytical){
 				//Schmitt, P. , Griswold, M. A., Jakob, P. M., Kotas, M. , Gulani, V. , Flentje, M. and Haase, A. (2004), 
@@ -258,9 +303,9 @@ int main_simPhantom(int argc, char* argv[argc])
 				}
 				
 			} else {	//start ODE based simulation
-				
+
 				ode_bloch_simulation3( &sim_data, mxySig, saR1Sig, saR2Sig, saDensSig, NULL );
-			
+
 				//Add data to phantom
 				for(int z = 0; z < dim_phantom[TE_DIM]; z++){
 					//changed x-and y-axis to have same orientation as measurements
@@ -274,7 +319,7 @@ int main_simPhantom(int argc, char* argv[argc])
         }
         
     }
-    
+
     //Calculate kspace of simulation
     if(kspace){
 		
@@ -285,22 +330,21 @@ int main_simPhantom(int argc, char* argv[argc])
 		unmap_cfl(DIMS, dim_phantom, ksp);
 		
 	}
-    
-    
+
     //Save created files 
-    
-    md_free(map_T1);
-	md_free(map_T2);
-	md_free(map_M0);
+	if (xdim != 1 && ydim != 1){
+		md_free(map_T1);
+		md_free(map_T2);
+		md_free(map_M0);
+	}
     unmap_cfl(DIMS, dim_phantom, phantom);    
     unmap_cfl(DIMS, dim_phantom, sensitivitiesT1);
     unmap_cfl(DIMS, dim_phantom, sensitivitiesT2);
 	unmap_cfl(DIMS, dim_phantom, sensitivitiesDens);
 	
-	
 	//Calculate and print elapsed time 
-    gettimeofday(&t2, NULL);
-    elapsedTime = ( t2.tv_sec - t1.tv_sec ) * 1000 + ( t2.tv_usec - t1.tv_usec ) / 1000;
+    gettimeofday(&t_end, NULL);
+    elapsedTime = ( t_end.tv_sec - t_start.tv_sec ) * 1000 + ( t_end.tv_usec - t_start.tv_usec ) / 1000;
     int min = (int) elapsedTime / 1000 / 60;
     int sec = ( ( (int) elapsedTime - min * 60 * 1000 ) / 1000) % 60;
     int msec =  (int) elapsedTime - min * 60 * 1000 - sec * 1000;
