81f4ea1db1d4ba8c59518541948e187d04115de4
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2019-07-08 16:57:55 +0200

    QuantMap:: Add RF test based on matrix exponential simulation.

--- utests/test_ode_simu.c
+++ utests/test_ode_simu.c
@@ -5,6 +5,7 @@
 #include "num/ode.h"
 #include "simu/bloch.h"
 #include "simu/simulation.h"
+#include "simu/sim_matrix.h"
 
 #include "num/multind.h"
 #include "num/flpmath.h"
@@ -414,4 +415,109 @@ static bool test_simulation(void)
 }
 
 UT_REGISTER_TEST(test_simulation);
+
+
+
+
+static bool test_RF_pulse_matexp(void)
+{
+#if 1
+	
+	
+	long dim[DIMS] = { [0 ... DIMS - 1] = 1 };
+    dim[0] = 10;
+    dim[1] = 10;
+	
+
+	float trfmin = 0.0001;
+	float trfmax = 0.1;
+	float amin = 1.;
+	float amax = 180.;
+	
+	for(int i = 0; i < dim[0]; i++ )
+		for(int j = 0; j < dim[1]; j++ ){
+			
+			float trf = (trfmin + (float)i/((float)dim[0] - 1.) * (trfmax - trfmin));
+			float angle = (amin + (float)j/((float)dim[1] - 1.) * (amax - amin));
+
+			struct SimData data;
+	
+			data.seqData = seqData_defaults;
+			data.seqData.seq_type = 1;
+			data.seqData.TR = 10;
+			data.seqData.TE = 5;
+			data.seqData.rep_num = 1;
+			data.seqData.spin_num = 1;
+			data.seqData.num_average_rep = 1;
+			
+			data.voxelData = voxelData_defaults;
+			data.voxelData.r1 = 0.;
+			data.voxelData.r2 = 0.;
+			data.voxelData.m0 = 1;
+			data.voxelData.w = 0;
+			
+			data.pulseData = pulseData_defaults;
+			data.pulseData.flipangle = angle;
+			data.pulseData.RF_end = trf;
+			data.gradData = gradData_defaults;
+			data.seqtmp = seqTmpData_defaults;
+			
+			data.pulseData.pulse_applied = true;
+				
+	
+			create_rf_pulse( &data.pulseData, 0, trf, angle, 0, 2, 2, 0.46 );
+			
+			enum { N = 10 };
+			
+			float x0[N] = { 0., 0., 1., 0., 0., 0., 0., 0., 0., 1. };
+			float x1[N];
+			
+			
+			//Starting first pulse
+			float m[N][N];
+			mat_exp_simu( N, data.pulseData.RF_end, m, &data);
+			
+			for (int i = 0; i < N; i++) {
+
+				x1[i] = 0.;
+
+				for (int j = 0; j < N; j++)
+					x1[i] += m[j][i] * x0[j];
+			}
+	
+			
+			float sim_angle = 0.;
+			
+			if( x1[2] >= 0 ){ //case of FA <= 90°
+				
+				if(data.voxelData.r1 != 0 && data.voxelData.r2 != 0)//for relaxation case
+					sim_angle = asinf(x1[1] / data.voxelData.m0) / M_PI * 180.;
+				else
+					sim_angle = asinf(x1[1] / sqrtf(x1[0]*x1[0]+x1[1]*x1[1]+x1[2]*x1[2])) / M_PI * 180.;
+			}
+			else {//case of FA > 90°
+				
+				if(data.voxelData.r1 != 0 && data.voxelData.r2 != 0)//for relaxation case
+					sim_angle = acosf( fabsf(x1[1]) / data.voxelData.m0 ) / M_PI * 180. + 90.;
+				else
+					sim_angle = acosf( fabsf(x1[1]) / sqrtf(x1[0]*x1[0]+x1[1]*x1[1]+x1[2]*x1[2]) ) / M_PI * 180. + 90.;
+			}
+			
+			
+			float err = fabsf( data.pulseData.flipangle - sim_angle );
+			
+			if (err > 10E-2)
+			{
+				debug_printf(DP_WARN, "Error in mat rf-pulse test!\n see -> utests/test_ode_simu.c\n");
+				return 0;
+			}
+		}
+		
+#endif
+	return 1;
+
+
+}
+
+UT_REGISTER_TEST(test_RF_pulse_matexp);
 
