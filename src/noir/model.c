e49ae666d8e75ab9b24c667d87a11125b170ca1b
xwang <xiaoqing.wang@med.uni-goettingen.de>
2019-07-17 21:43:31 +0200

    model.c: looks like the older implementation for sms chain is more efficient

--- src/noir/model.c
+++ src/noir/model.c
@@ -136,7 +136,15 @@ static struct noir_op_s* noir_init(const long dims[DIMS], const complex float* m
 	linop_free(wghts_ifft);
 
 
-	const struct linop_s* lop_fft = linop_fft_create(DIMS, data->data_dims, conf->fft_flags);
+	const struct linop_s* lop_fft = linop_fft_create(DIMS, data->data_dims, FFT_FLAGS);
+
+	if( dims[SLICE_DIM] != 1 ) { //SMS
+
+			const struct linop_s* tmp_fft = linop_fft_create_no_measure(DIMS, data->data_dims, SLICE_FLAG);
+			lop_fft = linop_chain(lop_fft, tmp_fft);
+			linop_free(tmp_fft);
+	}
+
 
 
 	data->ptr = md_alloc(DIMS, ptrn_dims, CFL_SIZE);
