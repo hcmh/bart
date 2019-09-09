168a99916eb55caaf6473140c8ab659085ed2485
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2019-09-09 16:51:48 +0200

    Implement analytical signal model for hsfp sequences

---
+++ src/simu/hsfp_model.h
@@ -0,0 +1,14 @@
+
+
+struct HSFP_model {
+	
+	float t1;
+	float t2;
+	float tr;
+	int repetitions;
+	float beta;
+	complex float* pa_profile; /*Polar angle */
+};
+extern const struct HSFP_model hsfp_defaults;
+
+void hsfp_simu(const struct HSFP_model* data, _Complex float* out);
