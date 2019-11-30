ff62ea81d529f66f240a11c498e16aaa2b637eee
Sebastian Rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2019-11-30 16:06:00 -0500

    num/qform: Add bilinear form

--- src/num/qform.c
+++ src/num/qform.c
@@ -1,4 +1,5 @@
 /* Copyright 2017. Martin Uecker. */
+/* Copyright 2019. Sebastian Rosenzweig. */
 
 #include <complex.h>
 #include <math.h>
@@ -54,4 +55,39 @@ void fit_quadratic_form(float qf[3], unsigned int N, const float phi[N], const f
 	qf[2] = out[2];
 }
 
+void fit_bilinear_form(float qf[3], unsigned int N, const float phi[N], const float v[N])
+{
+	complex float lhs[3] = { 0., 0., 0. };
+	complex float mat[3][3] = { { 0. } };
+
+	for (unsigned int i = 0; i < N; i++) {
+
+		float x = cosf(phi[i]);
+		float y = sinf(phi[i]);
+
+		lhs[0] += x * v[i];
+		lhs[1] += y * v[i];
+		lhs[2] += v[i];
+
+		mat[0][0] +=      x * x;
+		mat[0][1] +=      x * y;
+		mat[0][2] +=      x;
+		mat[1][0] +=      y * x;
+		mat[1][1] +=      y * y;
+		mat[1][2] +=      y;
+		mat[2][0] += 	  x;
+		mat[2][1] +=      y;
+		mat[2][2] +=      1;
+	}
+
+	complex float inv[3][3];
+	complex float out[3];
+
+	mat_inverse(3, inv, mat);
+	mat_vecmul(3, 3, out, inv, lhs);
+
+	qf[0] = out[0];
+	qf[1] = out[1];
+	qf[2] = out[2];
+}
 
