153acd48945375c9935025a6fc0ad84eae1a874e
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-02-05 10:12:51 +0100

    Create files to parse json to ellipsis struct.

---
+++ src/simu/read_json.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "simu/cJSON.h"
+
+#include "read_json.h"
+
+char* readfile(const char *filename)
+{
+	// open file for reading
+	FILE *f = NULL;
+	f = fopen(filename, "r");
+	
+	// determine its length
+	long len = 0;
+	fseek(f, 0, SEEK_END);
+	len = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *data = NULL;
+	data = (char*)malloc(len + 1);
+	
+	fread(data, 1, len, f);
+	data[len] = '\0';
+	fclose(f);
+
+	return data;
+}
+
+
+void read_json_to_struct(int N, struct ellipsis_s phantom[N], const cJSON* json_object)
+{
+	int i = 0;
+	const cJSON *iterate = NULL;
+	cJSON_ArrayForEach(iterate, json_object)
+	{
+		//cJSON *type = cJSON_GetObjectItemCaseSensitive(iterate, "type"); unused yet
+		
+		cJSON *intensity = cJSON_GetObjectItemCaseSensitive(iterate, "intensity");
+		phantom[i].intensity = intensity->valuedouble;
+		
+		const cJSON *axis = NULL;
+		axis = cJSON_GetObjectItemCaseSensitive(iterate, "axis");
+		
+		const cJSON *entries = NULL;
+		int j = 0;
+		cJSON_ArrayForEach(entries, axis)
+		{
+			phantom[i].axis[j] = entries->valuedouble;
+			j++;
+		}
+		
+		const cJSON *center = NULL;
+		center = cJSON_GetObjectItemCaseSensitive(iterate, "center");
+		
+		j = 0;
+		cJSON_ArrayForEach(entries, center)
+		{
+			phantom[i].center[j] = entries->valuedouble;
+			j++;
+		}
+		
+		cJSON *angle = cJSON_GetObjectItemCaseSensitive(iterate, "angle");
+		phantom[i].angle = angle->valuedouble;
+		
+		//cJSON *background = cJSON_GetObjectItemCaseSensitive(iterate, "background"); unused yet
+
+		i++;
+	}
+}
+
+void check_json_file(cJSON *json_file)
+{
+	if (json_file == NULL)
+	{
+		const char *error_ptr = cJSON_GetErrorPtr();
+		
+		if (error_ptr != NULL) 
+			fprintf(stderr, "Error before: %s\n", error_ptr);
+		
+		cJSON_Delete(json_file);
+	}
+}
---
+++ src/simu/read_json.h
@@ -0,0 +1,8 @@
+
+#include "simu/shepplogan.h"
+
+extern char* readfile(const char *filename);
+
+extern void read_json_to_struct(int N, struct ellipsis_s phantom[N], const cJSON* json_object);
+
+extern void check_json_file(cJSON *json_file);
