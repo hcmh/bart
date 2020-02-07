b4b24d3c8211bbf6315692202612623eb6eab3d4
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-02-07 14:10:04 +0100

    formatting sim.c

--- src/sim.c
+++ src/sim.c
@@ -50,19 +50,20 @@ static void help_seq(void)
 static bool opt_seq(void* ptr, char c, const char* optarg)
 {
 	(void) c;
-	
+
 	// Check if help function is called
 	char rt[5];
-	
+
 	int ret = sscanf(optarg, "%4[^:]", rt);
 	assert(1 == ret);
-	
+
 	if (strcmp(rt, "h") == 0) {
 
 		help_seq();
 		exit(0);
-	} else {
-		
+	}
+	else {
+
 		// Collect simulation data
 		struct sim_data* sim_data = ptr;
 
@@ -83,7 +84,7 @@ static bool opt_seq(void* ptr, char c, const char* optarg)
 int main_sim(int argc, char* argv[])
 {
 	int nbf = 8;
-	
+
 	// initalize values for simulation
 	struct sim_data sim_data;
 	sim_data.seq = simdata_seq_defaults;
@@ -91,10 +92,10 @@ int main_sim(int argc, char* argv[])
 	sim_data.pulse = simdata_pulse_defaults;
 	sim_data.grad = simdata_grad_defaults;
 	sim_data.tmp = simdata_tmp_defaults;
-	
-	
+
+
 	const struct opt_s opts[] = {
-		
+
 		OPT_INT('n', &nbf, "nbf", "No. Basis Functions"),
 		{ 'P', true, opt_seq, &sim_data, "\tA:B:C:D:E:F:G\tParameters for Simulation <Typ:Seq:tr:te:Drf:FA:#tr> (-Ph for help)" },
 	};
@@ -102,61 +103,59 @@ int main_sim(int argc, char* argv[])
 	cmdline(&argc, argv, 1, 1, usage_str, help_str, ARRAY_SIZE(opts), opts);
 
 	num_init();
-	
+
 	long dims[DIMS] = { [0 ... DIMS - 1] = 1 };
 	dims[TE_DIM] = sim_data.seq.rep_num;
 	dims[COEFF_DIM] = nbf;
-	
+
 	long dims1[DIMS];
 	md_select_dims(DIMS, TE_FLAG, dims1, dims);
-	
+
 	complex float* basis_functions = create_cfl(argv[1], DIMS, dims);
 	md_zfill(DIMS, dims, basis_functions, 1.0);
-	
-	
+
+
 	// Choose more realistic values (rand, manual and homogeneously distributed)
 	float t1[nbf];
 	float t2[nbf];
-	
+
 	for (int i = 0; i < nbf; i++) {
+
 		t1[i] = 3. - i * (3. - 0.1)/(float) nbf;
 		t2[i] = 1. - i * (1. - 0.04)/(float) nbf;
 	}
 
 	float m0 = 1.;
-	
+
 	// Apply simulation to all geometrical structures to determine time evolution of signal
 	#pragma omp parallel for
 	for (int j = 0; j < dims[COEFF_DIM]; j++) {
-		
+
 		struct sim_data data = sim_data;
 
 		data.voxel.r1 = 1/t1[j];
 		data.voxel.r2 = 1/t2[j];
 		data.voxel.m0 = m0;
-		
+
 		if (data.seq.analytical) {
-			
+
 			complex float* signal = md_alloc(DIMS, dims1, CFL_SIZE);
-			
+
 			if (5 == data.seq.seq_type)
-				
 				looklocker_analytical(&data, signal);
-			
 			else if (1 == data.seq.seq_type)
-				
 				IR_bSSFP_analytical(&data, signal);
 			else
-				
 				debug_printf(DP_ERROR, "Analytical function of desired sequence is not provided.\n");
-			
-			
+
+
 			for (int t = 0; t < dims[TE_DIM]; t++) 
 				basis_functions[j * dims[TE_DIM] + t] = signal[t];
-			
-			
-		} else { // TODO: change to complex floats!!
-			
+
+
+		} 
+		else { // TODO: change to complex floats!!
+
 			float mxy_sig[data.seq.rep_num / data.seq.num_average_rep][3];
 			float sa_r1_sig[data.seq.rep_num / data.seq.num_average_rep][3];
 			float sa_r2_sig[data.seq.rep_num / data.seq.num_average_rep][3];
@@ -164,14 +163,14 @@ int main_sim(int argc, char* argv[])
 
 			ode_bloch_simulation3(&data, mxy_sig, sa_r1_sig, sa_r2_sig, sa_m0_sig);	// ODE simulation
 // 			matrix_bloch_simulation(&data, mxy_sig, sa_r1_sig, sa_r2_sig, sa_m0_sig);	// OBS simulation, does not work with hard-pulses!
-			
+
 			for (int t = 0; t < dims[TE_DIM]; t++) 
 				basis_functions[j * dims[TE_DIM] + t] = mxy_sig[t][1] + mxy_sig[t][0] * I;
 		}
 	}
-	
+
 	unmap_cfl(DIMS, dims, basis_functions );
-	
+
 	return 0;
 }
 
