05d3170e2eab0d2fa29f07ba262dda0e60f06d4e
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-02-12 23:57:44 +0100

    pulse: use mid point, maybe this is right

--- src/simu/pulse.c
+++ src/simu/pulse.c
@@ -12,19 +12,20 @@
 
 float pulse_energy(const struct simdata_pulse* pulse)
 {	
-	// Assuming pulse starts at t=0
-
 	float c = M_PI / pulse->n / pulse->t0;
 	float d = M_PI / pulse->t0;
 
-	float si0 = Si( d * (pulse->rf_end / 2.));
-	float si1 = Si(-d * (pulse->rf_end / 2.));
-	float si2 = Si( (c - d) * pulse->rf_end / 2.);
-	float si3 = Si(-(c - d) * pulse->rf_end / 2.);
-	float si4 = Si( (c + d) * pulse->rf_end / 2.);
-	float si5 = Si(-(c + d) * pulse->rf_end / 2.);
+	float mid = (pulse->rf_end - pulse->rf_start) / 2.;
 
-	return pulse->A * (1. - pulse->alpha) / d * (si0 - si1) + pulse->A * pulse->alpha / (2. * d) * (si2 - si3 + si4 - si5);
+	float si0 = Si( d * mid);
+	float si1 = Si(-d * mid);
+	float si2 = Si( (c - d) * mid);
+	float si3 = Si(-(c - d) * mid);
+	float si4 = Si( (c + d) * mid);
+	float si5 = Si(-(c + d) * mid);
+
+	return    pulse->A * (1. - pulse->alpha) / d * (si0 - si1)
+		+ pulse->A * pulse->alpha / (2. * d) * (si2 - si3 + si4 - si5);
 }
 
 
