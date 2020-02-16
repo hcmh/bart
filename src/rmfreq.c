ea1cdb2c177f7f915a3932b5cb645c8d84e22547
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-02-16 19:03:43 +0100

    white space rmfreq

--- src/rmfreq.c
+++ src/rmfreq.c
@@ -4,11 +4,10 @@
  *
  * Authors:
  * 2019 Sebastian Rosenzweig
- * 
- * Manuscript:
- * Sebastian Rosenzweig, Nick Scholand, H. Christian M. Holme, Martin Uecker. Cardiac and Respiratory Self-Gating in Radial MRI using an Adapted Singular Spectrum Analysis (SSA-FARY)
  *
- *
+ * Sebastian Rosenzweig, Nick Scholand, H. Christian M. Holme, Martin Uecker.
+ * Cardiac and Respiratory Self-Gating in Radial MRI using an Adapted Singular
+ * Spectrum Analysis (SSA-FARY)
  */
 
 #include <stdbool.h>
@@ -29,8 +28,9 @@
 
 #include "noncart/traj.h"
 
+
 static const char usage_str[] = "<traj> <k> <k_cor>";
-static const char help_str[] = "Remove angle-dependent frequency \n";
+static const char help_str[] = "Remove angle-dependent frequency\n";
 
 
 
@@ -39,6 +39,7 @@ int main_rmfreq(int argc, char* argv[])
 	unsigned int n_harmonics = 5;
 
 	const struct opt_s opts[] = {
+
 		OPT_UINT('N', &n_harmonics, "#", "Number of harmonics [Default: 5]"),
 	};
 
@@ -50,13 +51,13 @@ int main_rmfreq(int argc, char* argv[])
 	long k_dims[DIMS];
 	complex float* k = load_cfl(argv[2], DIMS, k_dims);
 
-	if ( md_check_dimensions(DIMS, k_dims, COIL_FLAG|TIME_FLAG|SLICE_FLAG) )
+	if (md_check_dimensions(DIMS, k_dims, COIL_FLAG|TIME_FLAG|SLICE_FLAG))
 		error("Only COIL_DIM, TIME_DIM and SLICE_DIM may have entries!\n");
 
 	// Read trajectory
 	long t_dims[DIMS];
 	complex float* t = load_cfl(argv[1], DIMS, t_dims);
-	
+
 	if (!md_check_equal_dims(DIMS, t_dims, k_dims, ~(READ_FLAG|PHS1_FLAG|COIL_FLAG)))
 		error("k-space and trajectory inconsistent!\n");
 
@@ -64,12 +65,14 @@ int main_rmfreq(int argc, char* argv[])
 	// Calculate angles from trajectory	
 	long angles_dims[DIMS];
 	md_select_dims(DIMS, ~(PHS1_FLAG|COIL_FLAG), angles_dims, k_dims);
+
 	complex float* angles = md_alloc(DIMS, angles_dims, CFL_SIZE);
 
 	long t1_dims[DIMS];
 	md_select_dims(DIMS, ~MD_BIT(1), t1_dims, t_dims);
 
 	complex float* t1 = md_alloc(DIMS, t1_dims, CFL_SIZE);
+
 	md_slice(DIMS, MD_BIT(1), (long[DIMS]){ 0 }, t_dims, t1, t, CFL_SIZE);
 
 	int N = 1;
@@ -77,12 +80,15 @@ int main_rmfreq(int argc, char* argv[])
 		N = N * angles_dims[i];
 
 	for (int i = 0; i < N; i++) {
-		angles[i] = M_PI + atan2f(crealf(t1[3 * i + 0]), crealf(t1[3 * i + 1])) + 0. * I;
+
+		angles[i] = M_PI + atan2f(crealf(t1[3 * i + 0]), crealf(t1[3 * i + 1]));
+
  		//debug_printf(DP_INFO, "i: %d, angle: %f\n", i, creal(angles[i]) * 360. / 2. / M_PI);
 	}
-	
+
 	// negative angles
 	complex float* neg_angles = md_alloc(DIMS, angles_dims, CFL_SIZE);
+
 	md_zsmul(DIMS, angles_dims, neg_angles, angles, -1.);
 
 	// Projection matrix
@@ -97,52 +103,67 @@ int main_rmfreq(int argc, char* argv[])
 	long pos[DIMS] = { 0 };
 
 	int count = 0;
+
 	for (unsigned int h = 0; h < n_harmonics; h++) {
+
 		// exp(i * theta * (h+1))
 		md_zsmul(DIMS, angles_dims, angles1, angles, (h + 1));
 		md_zexpj(DIMS, angles_dims, n_singleton, angles1);
+
 		pos[15] = count;
 		md_copy_block(DIMS, pos, n_dims, n, angles_dims, n_singleton, CFL_SIZE);
-		count ++;
+		count++;
 
 		// exp(- i * theta * (h+1))
 		md_zsmul(DIMS, angles_dims, angles1, neg_angles, (h + 1));
 		md_zexpj(DIMS, angles_dims, n_singleton, angles1);
+
 		pos[15] = count;
+
 		md_copy_block(DIMS, pos, n_dims, n, angles_dims, n_singleton, CFL_SIZE);
-		count ++;
+		count++;
 	}
 
 	// Projection
 	long k_singleton_dims[DIMS];
 	md_select_dims(DIMS, TIME_FLAG, k_singleton_dims, k_dims);
+
 	complex float* k_singleton = md_alloc(DIMS, k_singleton_dims, CFL_SIZE);
+
 
 	long n_part_singleton_dims[DIMS];
 	md_select_dims(DIMS, ~SLICE_FLAG, n_part_singleton_dims, n_dims);
+
 	complex float* n_part_singleton = md_alloc(DIMS, n_part_singleton_dims, CFL_SIZE);
+
 
 	long pinv_dims[DIMS];
 	md_transpose_dims(DIMS, 10, 15, pinv_dims, n_part_singleton_dims);
+
 	complex float* pinv = md_alloc(DIMS, pinv_dims, CFL_SIZE);
 
 	long proj_dims[DIMS];
 	for (unsigned int i = 0; i < DIMS; i++)
 		proj_dims[i] = 1;
+
 	proj_dims[0] = 2 * n_harmonics;
 
 	complex float* proj = md_alloc(DIMS, proj_dims, CFL_SIZE);
 	complex float* proj_singleton = md_alloc(DIMS, k_singleton_dims, CFL_SIZE);
 	complex float* k_cor_singleton = md_alloc(DIMS, k_singleton_dims, CFL_SIZE);
+
 	complex float* k_cor = create_cfl(argv[3], DIMS, k_dims);
 
 	long pos1[DIMS] = { 0 };
 
 	// Coil-by-coil, Partition-by-Partition correction
-	for (int c = 0; c < k_dims[COIL_DIM] ; c++) {
+	for (int c = 0; c < k_dims[COIL_DIM]; c++) {
+
 		for (int p = 0; p < k_dims[SLICE_DIM]; p++) {
+
 			pos1[SLICE_DIM] = p;
 			pos1[COIL_DIM] = 0;
+
 			md_copy_block(DIMS, pos1, n_part_singleton_dims, n_part_singleton, n_dims,  n, CFL_SIZE);
 
 			pos1[COIL_DIM] = c;
@@ -179,7 +200,6 @@ int main_rmfreq(int argc, char* argv[])
 
 	unmap_cfl(DIMS, k_dims, k);
 	unmap_cfl(DIMS, k_dims, k_cor);
-
 
 	exit(0);
 }
