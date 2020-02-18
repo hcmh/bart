a6dc751690f81a4d8d66a08713cc654207b30e53
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-02-18 08:57:00 +0100

    Update sine integral utest.

--- utests/test_specfun.c
+++ utests/test_specfun.c
@@ -4,9 +4,25 @@
 #include "utest.h"
 
 static bool test_sine_integral(void)
-{	
-	// TODO: Add more tests using various values
-	return (Si(1.) - 0.946083070367183) < 10E-5;
+{
+	double tests[][2] = {	{ 0.,	0. },
+				{ 1.,	0.946083 },
+				{ M_PI,	1.851937 },/* Wilbraham-Gibbs constant*/
+				{ 10,	1.658348 },
+				{ 100,	1.562226 }
+	};
+
+	for (unsigned int i = 0; i < ARRAY_SIZE(tests); i++) {
+
+		if ( (Si(tests[i][0]) - tests[i][1]) > 10E-5)
+			return 0;
+
+		// Test for Si(-z) = -Si(z)
+		if ( (Si(-tests[i][0]) + tests[i][1]) > 10E-5)
+			return 0;
+	}
+
+	return 1;
 }
 
 UT_REGISTER_TEST(test_sine_integral);
