a4ac2df5f154a1c37f4c111513e592d62c9eeed9
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-03-03 15:05:19 +0100

    Fix multiple GA option in traj tool.

--- src/noncart/traj.c
+++ src/noncart/traj.c
@@ -136,7 +136,7 @@ void calc_base_angles(double base_angle[DIMS], int Y, int E, int mb, int turns, 
 	 */
 
 	double golden_ratio = (sqrtf(5.) + 1.) / 2;
-	double golden_angle = conf.multiple_ga * M_PI / (golden_ratio + conf.tiny_gold - 1.);
+	double golden_angle = M_PI / (golden_ratio + conf.tiny_gold - 1.);
 
 	// For numerical stability
 	if (1 == conf.tiny_gold)
@@ -146,6 +146,8 @@ void calc_base_angles(double base_angle[DIMS], int Y, int E, int mb, int turns, 
 
 	if (conf.rational)
 		golden_angle = rational_angle(Y, conf.tiny_gold);
+
+	golden_angle *= conf.multiple_ga;
 
 	// Angle between spokes of one slice/partition
 	double angle_s = angle_atom * (conf.full_circle ? 2 : 1);
