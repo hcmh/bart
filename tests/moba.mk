bb8659be9b157751f4630145bda47d0543ad130d
xwang <xiaoqing.wang@med.uni-goettingen.de>
2020-03-26 13:21:40 +0100

    test: add the moba test for non-IR FLASH.

--- tests/moba.mk
+++ tests/moba.mk
@@ -38,8 +38,25 @@ tests/test-moba-t1-sms: phantom signal fft ones index scale moba looklocker fmac
 	touch $@
 
 
+tests/test-moba-t1-no-IR: phantom signal fft ones index scale moba looklocker fmac scale nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra 		                  		;\
+	$(TOOLDIR)/signal -F -r0.005 -n300 -1 1.12:1.12:1 -2 100:100:1 signal.ra	;\
+	$(TOOLDIR)/fmac circ.ra signal.ra image.ra					;\
+	$(TOOLDIR)/fft 3 image.ra k_space.ra						;\
+	$(TOOLDIR)/ones 6 16 16 1 1 1 300 psf.ra					;\
+	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
+	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                    	       		;\
+	$(TOOLDIR)/moba -L -i11 -f1 -C200 -p psf.ra k_space.ra TI.ra reco.ra		;\
+	$(TOOLDIR)/looklocker -t0.1 -D0. reco.ra T1.ra		    			;\
+	$(TOOLDIR)/fmac T1.ra circ.ra masked.ra		    				;\
+	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra			    			;\
+	$(TOOLDIR)/nrmse -t 0.005 masked.ra ref.ra			    		;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
 
-TESTS += tests/test-moba-t1 tests/test-moba-t1-sms
+
+TESTS += tests/test-moba-t1 tests/test-moba-t1-sms tests/test-moba-t1-no-IR
 
 
