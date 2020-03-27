aad1fbb929c41941392bf6c60cf7f045392f045a
rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2020-03-27 14:03:53 +0100

    pad: Init

---
+++ src/pad.c
@@ -0,0 +1,118 @@
+/* Copyright 2013-2014. The Regents of the University of California.
+ * Copyright 2015-2016. Martin Uecker.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
+ * 
+ * Authors:
+ * 2020 Sebastian Rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
+ */
+
+#include <complex.h>
+#include <stdbool.h>
+
+#include "num/multind.h"
+#include "num/init.h"
+
+#include "misc/mmio.h"
+#include "misc/misc.h"
+#include "misc/opts.h"
+
+
+
+#ifndef DIMS
+#define DIMS 16
+#endif
+
+#ifndef CFL_SIZE
+#define CFL_SIZE sizeof(complex float)
+#endif
+
+
+static const char usage_str[] = "dim len [val] <input> <output>";
+static const char help_str[] = "Pad an array along dimension by value [val] or by edge values.";
+
+
+int main_pad(int argc, char* argv[])
+{
+	bool asym = false;
+	bool pad_by_value = false;
+
+	const struct opt_s opts[] = {
+
+		OPT_SET('a', &asym, "Asymmetric padding"),
+
+	};
+
+	cmdline(&argc, argv, 4, 5, usage_str, help_str, ARRAY_SIZE(opts), opts);
+
+	num_init();
+
+    int dim = atoi(argv[1]);
+    assert(dim >= 0 && dim < DIMS);
+
+    int len = atoi(argv[2]);
+    assert(len > 0);
+
+    complex float val = 0;
+	if (argc == 6) {
+		parse_cfl(&val, argv[3]);
+		pad_by_value = true;
+	}
+
+	long in_dims[DIMS];
+	long out_dims[DIMS];
+
+	void* in_data = load_cfl(argv[argc - 2], DIMS, in_dims);
+	md_copy_dims(DIMS, out_dims, in_dims);
+
+	out_dims[dim] = in_dims[dim] + (asym ? len : 2 * len); // padding on one end or both
+
+	void* out_data = create_cfl(argv[argc - 1], DIMS, out_dims);
+
+	// Assign position of in_data and pad by value
+    if (asym)
+        md_pad(DIMS, &val, out_dims, out_data, in_dims, in_data, CFL_SIZE);
+    else
+        md_pad_center(DIMS, &val, out_dims, out_data, in_dims, in_data, CFL_SIZE);
+
+	// Overwrite padding by edge value
+	if (!pad_by_value) {
+
+		long in_strs[DIMS];
+        md_calc_strides(DIMS, in_strs, in_dims, CFL_SIZE);
+
+        long in_mod_strs[DIMS];
+        md_calc_strides(DIMS, in_mod_strs, in_dims, CFL_SIZE);
+        in_mod_strs[dim] = 0;
+
+        long pad_dims[DIMS];
+        md_copy_dims(DIMS, pad_dims, in_dims);
+        pad_dims[dim] = len;
+
+        long out_strs[DIMS]; 
+        md_calc_strides(DIMS, out_strs, out_dims, CFL_SIZE); 
+
+        long pos[DIMS] = { 0 };
+		long pos1[DIMS] = { 0 };
+		pos1[dim] = in_dims[dim] - 1;
+		long offset_in = md_calc_offset(DIMS, in_strs, pos1); 
+		
+		pos1[dim] = in_dims[dim] + (asym ? 0 : len);
+		long offset_out = md_calc_offset(DIMS, out_strs, pos1); 
+		
+		md_copy_block2(DIMS, pos, pad_dims, out_strs, out_data + offset_out, pad_dims, in_mod_strs, in_data + offset_in, CFL_SIZE); 
+	
+		if (!asym)
+         	md_copy_block2(DIMS, pos, pad_dims, out_strs, out_data, pad_dims, in_mod_strs, in_data, CFL_SIZE); 
+		
+
+	}
+	
+	unmap_cfl(DIMS, in_dims, in_data);
+	unmap_cfl(DIMS, out_dims, out_data);
+
+	return 0;
+
+}
+
+
