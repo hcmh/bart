6d86f939ca8cd76670b1629edbc41e70f09d67d8
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2020-03-27 10:54:48 +0100

    moba_test: add an non-Cartesian case

--- tests/moba.mk
+++ tests/moba.mk
@@ -73,9 +73,32 @@ tests/test-moba-t1-no-IR: phantom signal fft ones index scale moba looklocker fm
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-moba-t1-nonCartesian: traj transpose phantom signal nufft fft ones index scale moba looklocker resize fmac scale nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
+	$(TOOLDIR)/traj -x32 -y1 -r -D -G -s7 -t300 _traj.ra  		                ;\
+	$(TOOLDIR)/transpose 5 10 _traj.ra _traj2.ra    		                ;\
+	$(TOOLDIR)/scale 0.5 _traj2.ra traj.ra   	    		                ;\
+	$(TOOLDIR)/phantom -k -c -t traj.ra basis_geom.ra    	    		        ;\
+	$(TOOLDIR)/signal -F -I -r0.005 -n300 -1 1.12:1.12:1 -2 100:100:1 signal.ra	;\
+	$(TOOLDIR)/fmac -s 64 basis_geom.ra signal.ra data.ra				;\
+ 	$(TOOLDIR)/ones 16 1 32 1 1 1 300 1 1 1 1 1 1 1 1 1 1 ones.ra	   		;\
+	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra ones.ra pattern.ra	   		;\
+	$(TOOLDIR)/fft -u 3 pattern.ra psf.ra				   		;\
+	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra data.ra zerofill_reco.ra  		;\
+	$(TOOLDIR)/fft -u 3 zerofill_reco.ra k_space.ra					;\
+	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
+	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                    	       		;\
+	$(TOOLDIR)/moba -L -l1 -i11 -C200 -j0.01 -p psf.ra k_space.ra TI.ra reco.ra		;\
+	$(TOOLDIR)/looklocker -t0.1 -D0. reco.ra T1.ra		    			;\
+	$(TOOLDIR)/resize -c 0 16 1 16 T1.ra T1_crop.ra					;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra						;\
+	$(TOOLDIR)/fmac T1_crop.ra circ.ra masked.ra	    				;\
+	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra			    			;\
+	$(TOOLDIR)/nrmse -t 0.02 masked.ra ref.ra			    		;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
 
 TESTS += tests/test-moba-t1 tests/test-moba-t1-sms tests/test-moba-t1-no-IR
-TESTS += tests/test-moba-t1-magn
-
+TESTS += tests/test-moba-t1-magn tests/test-moba-t1-nonCartesian
 
