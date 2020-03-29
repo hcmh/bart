3dd12e01f9e093d72e5c5fddc380ac2fdd79348e
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-03-29 17:34:36 +0200

    Update sim tests to new bash handling.

--- tests/sim.mk
+++ tests/sim.mk
@@ -1,16 +1,16 @@
 
 tests/test-sim-analy-irbssfp: sim nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000 analytical.ra	;\
-	$(TOOLDIR)/sim -P 0:1:0.0045:0.00225:0.00001:45:1000 simu.ra		;\
+	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000 -1 3:3:1 -2 1:1:1 analytical.ra	;\
+	$(TOOLDIR)/sim -P 0:1:0.0045:0.00225:0.00001:45:1000 -1 3:3:1 -2 1:1:1 simu.ra		;\
 	$(TOOLDIR)/nrmse -t 0.008 analytical.ra simu.ra			    		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 tests/test-sim-analy-flash: sim cabs mip spow fmac nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 1:2:0.0041:0.00258:0.001:6:1000 analytical.ra		;\
-	$(TOOLDIR)/sim -P 0:2:0.0041:0.00258:0.001:6:1000 _simu.ra			;\
+	$(TOOLDIR)/sim -P 1:2:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 analytical.ra		;\
+	$(TOOLDIR)/sim -P 0:2:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 _simu.ra			;\
 	$(TOOLDIR)/cabs _simu.ra _simu_abs.ra								;\
 	$(TOOLDIR)/mip 32 _simu_abs.ra max.ra								;\
 	$(TOOLDIR)/spow -- -1 max.ra scale.ra								;\
@@ -21,8 +21,8 @@ tests/test-sim-analy-flash: sim cabs mip spow fmac nrmse
 
 tests/test-sim-analy-irflash: sim cabs mip spow fmac nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 1:5:0.0041:0.00258:0.001:6:1000 analytical.ra		;\
-	$(TOOLDIR)/sim -P 0:5:0.0041:0.00258:0.001:6:1000 _simu.ra			;\
+	$(TOOLDIR)/sim -P 1:5:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 analytical.ra		;\
+	$(TOOLDIR)/sim -P 0:5:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 _simu.ra			;\
 	$(TOOLDIR)/cabs _simu.ra _simu_abs.ra								;\
 	$(TOOLDIR)/mip 32 _simu_abs.ra max.ra								;\
 	$(TOOLDIR)/spow -- -1 max.ra scale.ra								;\
@@ -33,32 +33,32 @@ tests/test-sim-analy-irflash: sim cabs mip spow fmac nrmse
 
 tests/test-sim-ode-bssfp: sim nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 0:0:0.0045:0.00225:0.001:45:1000 simu_obs.ra		;\
-	$(TOOLDIR)/sim -o -P 0:0:0.0045:0.00225:0.001:45:1000 simu_ode.ra	;\
+	$(TOOLDIR)/sim -P 0:0:0.0045:0.00225:0.001:45:1000 -1 3:3:1 -2 1:1:1 simu_obs.ra		;\
+	$(TOOLDIR)/sim -o -P 0:0:0.0045:0.00225:0.001:45:1000 -1 3:3:1 -2 1:1:1 simu_ode.ra	;\
 	$(TOOLDIR)/nrmse -t 0.005 simu_obs.ra simu_ode.ra			    	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 tests/test-sim-ode-irbssfp: sim nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 0:1:0.0045:0.00225:0.001:45:1000 simu_obs.ra		;\
-	$(TOOLDIR)/sim -o -P 0:1:0.0045:0.00225:0.001:45:1000 simu_ode.ra	;\
+	$(TOOLDIR)/sim -P 0:1:0.0045:0.00225:0.001:45:1000 -1 3:3:1 -2 1:1:1 simu_obs.ra		;\
+	$(TOOLDIR)/sim -o -P 0:1:0.0045:0.00225:0.001:45:1000 -1 3:3:1 -2 1:1:1 simu_ode.ra	;\
 	$(TOOLDIR)/nrmse -t 0.003 simu_obs.ra simu_ode.ra			    	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 tests/test-sim-ode-flash: sim nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 0:2:0.0041:0.00258:0.001:6:1000 simu_obs.ra		;\
-	$(TOOLDIR)/sim -o -P 0:2:0.0041:0.00258:0.001:6:1000 simu_ode.ra	;\
+	$(TOOLDIR)/sim -P 0:2:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 simu_obs.ra		;\
+	$(TOOLDIR)/sim -o -P 0:2:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 simu_ode.ra	;\
 	$(TOOLDIR)/nrmse -t 0.002 simu_obs.ra simu_ode.ra			    	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 tests/test-sim-ode-irflash: sim nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/sim -P 0:5:0.0041:0.00258:0.001:6:1000 simu_obs.ra		;\
-	$(TOOLDIR)/sim -o -P 0:5:0.0041:0.00258:0.001:6:1000 simu_ode.ra	;\
+	$(TOOLDIR)/sim -P 0:5:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 simu_obs.ra		;\
+	$(TOOLDIR)/sim -o -P 0:5:0.0041:0.00258:0.001:6:1000 -1 3:3:1 -2 1:1:1 simu_ode.ra	;\
 	$(TOOLDIR)/nrmse -t 0.001 simu_obs.ra simu_ode.ra			    	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
