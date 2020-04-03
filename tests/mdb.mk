ccc7a7f8c4c6f0cd67d4eb43dadf7050315b7314
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-04-03 12:08:30 +0200

    Add PSF test for mdb Bloch

--- tests/mdb.mk
+++ tests/mdb.mk
@@ -18,6 +18,30 @@ tests/test-mdb-bloch: phantom sim fmac fft ones modbloch slice scale nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-mdb-bloch-psf: traj repmat phantom sim fmac ones nufft fft modbloch slice scale nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/traj -x16 -y16 _traj.ra			;\
+	$(TOOLDIR)/repmat 5 1000 _traj.ra traj.ra	;\
+	$(TOOLDIR)/phantom -c -k -t traj.ra basis_geom.ra				;\
+	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
+	$(TOOLDIR)/fmac basis_geom.ra basis_simu.ra k_space.ra		;\
+	$(TOOLDIR)/ones 6 1 16 16 1 1 1000 ones.ra		;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a traj.ra ones.ra psf.ra	;\
+	$(TOOLDIR)/fft -u 3 psf.ra pattern.ra	;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a traj.ra k_space.ra zerofill.ra		;\
+	$(TOOLDIR)/fft -u 3 zerofill.ra k_grid.ra	;\
+	$(TOOLDIR)/modbloch -P 1:0.0045:0.00225:45:0.00001:0.00001:0.00001 -R2 -f1 -o0 -l1 -n0 -i15 -a1 -m0 -r0 -p pattern.ra k_grid.ra reco.ra sens.ra	;\
+	$(TOOLDIR)/slice 6 0 reco.ra t1map.ra				;\
+	$(TOOLDIR)/phantom -x 16 -c ref_geom.ra				;\
+	$(TOOLDIR)/fmac t1map.ra ref_geom.ra masked_t1.ra				;\
+	$(TOOLDIR)/scale -- 1.12 ref_geom.ra ref_t1.ra				;\
+	$(TOOLDIR)/nrmse -t 0.007 masked_t1.ra ref_t1.ra				;\
+	$(TOOLDIR)/slice 6 1 reco.ra t2map.ra				;\
+	$(TOOLDIR)/fmac t2map.ra ref_geom.ra masked_t2.ra				;\
+	$(TOOLDIR)/scale -- 0.1 ref_geom.ra ref_t2.ra				;\
+	$(TOOLDIR)/nrmse -t 0.005 masked_t2.ra ref_t2.ra				;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 tests/test-mdb-bloch-multi-tube: phantom sim fmac fft ones modbloch transpose slice mip scale nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
 	$(TOOLDIR)/phantom -x 20 -T -b basis_geom.ra				;\
