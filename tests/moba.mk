2dd53f19122d3ed9005b4d224209aecd03e1cb00
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2020-04-03 08:54:00 +0200

    add a test function for moba T1 reco with irgnm-admm

--- tests/moba.mk
+++ tests/moba.mk
@@ -138,7 +138,25 @@ tests/test-moba-t1-MOLLI: phantom signal fft ones index scale moba slice invert 
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-moba-t1-irgnm-admm: phantom signal fft ones index scale moba slice invert resize fmac nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
+	$(TOOLDIR)/phantom -x32 -c circ.ra 		                  		;\
+	$(TOOLDIR)/signal -I -F -r0.005 -n600 -1 1.12:1.12:1 -2 100:100:1 signal.ra     ;\
+	$(TOOLDIR)/fmac circ.ra signal.ra image.ra					;\
+	$(TOOLDIR)/fft 3 image.ra k_space.ra						;\
+	$(TOOLDIR)/ones 6 32 32 1 1 1 600 psf.ra					;\
+	$(TOOLDIR)/index 5 600 tmp1.ra   						;\
+	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                  	       			;\
+	$(TOOLDIR)/moba -L -l3 -i11 -f1 -p psf.ra k_space.ra TI.ra reco.ra	        ;\
+	$(TOOLDIR)/looklocker -t0.1 -D0. reco.ra T1.ra					;\
+	$(TOOLDIR)/fmac T1.ra circ.ra masked.ra						;\
+	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra						;\
+	$(TOOLDIR)/nrmse -t 0.0004 masked.ra ref.ra					;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
 TESTS += tests/test-moba-t1 tests/test-moba-t1-sms tests/test-moba-t1-no-IR
 TESTS += tests/test-moba-t1-magn tests/test-moba-t1-nonCartesian tests/test-moba-t1-nufft
 TESTS += tests/test-moba-t1-MOLLI
+TESTS += tests/test-moba-t1-irgnm-admm
 
