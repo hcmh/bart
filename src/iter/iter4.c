68076babe45c0204acf49961e5347bbd25700043
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2020-04-03 09:44:37 +0200

    add change of inverse2 in iter4

--- src/iter/iter4.c
+++ src/iter/iter4.c
@@ -182,7 +182,17 @@ static void inverse2(iter_op_data* _data, float alpha, float* dst, const float* 
 
 	iter_op_call(data->adj, tmp, src);
 
-	inverse(_data, alpha, dst, tmp);
+	float eps = data->cgtol * md_norm(1, MD_DIMS(data->size), src);
+
+
+	/* The original (Matlab) nlinv implementation uses
+	 * "sqrt(rsnew) < 0.01 * rsnot" as termination condition.
+	 */
+	if (data->nlinv_legacy)
+		eps = powf(eps, 2.);
+
+        conjgrad(data->cgiter, alpha, eps, data->size, select_vecops(src),
+			(struct iter_op_s){ normal, CAST_UP(data) }, dst, tmp, NULL);
 
 	md_free(tmp);
 }
