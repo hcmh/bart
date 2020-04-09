f0dc67a238dc660266ca20403c21a565b76e9407
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-04-09 20:11:29 +0200

    add lang.h header for language features

---
+++ src/misc/lang.h
@@ -0,0 +1,12 @@
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <complex.h>
+#include <assert.h>
+
+#include "nested.h"
+#include "types.h"
+
+#define with(...) switch (1) default: for (__VA_ARGS__ , ({ break; }))
+
+
