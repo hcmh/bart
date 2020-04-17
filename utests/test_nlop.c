c5649783f00380263219cf220e24f57d42b0d17f
xwang <xiaoqing.wang@med.uni-goettingen.de>
2020-04-17 11:44:38 +0200

    Revert "turn off failing test"
    This reverts commit e4ea670b22e2433ee2ebbb7f61fde55e9dfe064a.

--- utests/test_nlop.c
+++ utests/test_nlop.c
@@ -773,5 +773,5 @@ static bool test_nlop_parallel_derivatives(void)
 	return result;
 }
 
-// FIXME U T _REGISTER_TEST(test_nlop_parallel_derivatives);
+UT_REGISTER_TEST(test_nlop_parallel_derivatives);
 
