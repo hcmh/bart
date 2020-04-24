abab55476eb3424b68def120328031e3077a2c84
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-04-24 14:47:23 +0200

    Update phantom basis function test.

--- tests/phantom.mk
+++ tests/phantom.mk
@@ -62,7 +62,7 @@ tests/test-phantom-bart: fft nrmse phantom
 tests/test-phantom-basis: nrmse phantom fmac
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
 	$(TOOLDIR)/phantom -T -k k0.ra								;\
-	$(TOOLDIR)/phantom -T -b -k k1.ra							;\
+	$(TOOLDIR)/phantom -T -b 11 -k k1.ra							;\
 	$(TOOLDIR)/fmac -s 64 k1.ra k2.ra							;\
 	$(TOOLDIR)/nrmse -t 0.000001 k0.ra k2.ra						;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
