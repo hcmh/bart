544c00676a151ba9bebe986fb8777fc6665a7f96
mblum <moritz.blumenthal@med.uni-goettingen.de>
2020-05-01 15:42:14 +0200

    Add gemv blas wrappers

--- src/num/blas.c
+++ src/num/blas.c
@@ -71,3 +71,51 @@ void (blas_csyrk)(char uplo, char trans, long N, long K, const complex float alp
 
 	cblas_csyrk(CblasColMajor, CblasUpper, ('T' == trans) ? CblasTrans : CblasNoTrans, N, K, (void*)&alpha, (void*)A, lda, (void*)&beta, (void*)C, ldc);
 }
+
+void blas_cgemv(char trans, long M, long N, complex float alpha, long lda, const complex float A[N][lda], long incx, const complex float* x, complex float beta, long incy, complex float* y)
+{
+#ifdef USE_CUDA
+#define CUCOMPLEX(x) (((union { cuComplex cu; complex float std; }){ .std = (x) }).cu)
+        if (cuda_ondevice(A)) {
+
+                cublasCgemv	(trans, M, N, CUCOMPLEX(alpha),
+                                (const cuComplex*)A, lda,
+                                (const cuComplex*)x, incx,
+                                CUCOMPLEX(beta), (cuComplex*)y, incy);
+        } else
+#endif
+        cblas_cgemv(	CblasColMajor, ('T' == trans) ? CblasTrans : (('C' == trans) ? CblasConjTrans : CblasNoTrans), M, N, (void*)&alpha,
+			(void*)A, lda,
+			(void*)x, incx,
+			(void*)&beta, (void*)y, incy);
+}
+
+void (blas_gemv_zfmac)(long M, long N, complex float* y, const complex float* A, char trans, const complex float* x)
+{
+    	assert((trans == 'N') || (trans == 'T') || (trans == 'C'));
+	blas_cgemv(trans,M, N, 1., M, *(complex float (*)[N][M])A, 1, x, 1., 1, y);
+}
+
+void blas_sgemv(char trans, long M, long N, float alpha, long lda, const float A[N][lda], long incx, const float* x, float beta, long incy, float* y)
+{
+#ifdef USE_CUDA
+
+        if (cuda_ondevice(A)) {
+
+                cublasSgemv(	trans, M, N, alpha,
+                                (const float*)A, lda,
+                                x, incx,
+                                beta, y, incy);
+        } else
+#endif
+        cblas_sgemv(	CblasColMajor, ('T' == trans) ? CblasTrans : CblasNoTrans, M, N, alpha,
+			(const float*)A, lda,
+			x, incx,
+			beta, y, incy);
+}
+
+void (blas_gemv_fmac)(long M, long N, float* y, const float* A, char trans, const float* x)
+{
+    	assert((trans == 'N') || (trans == 'T'));
+	blas_sgemv(trans,M, N, 1., M, *(float (*)[N][M])A, 1, x, 1., 1, y);
+}
--- src/num/blas.h
+++ src/num/blas.h
@@ -8,3 +8,8 @@ extern void blas_matrix_zfmac(long M, long K, long N, complex float* C, const co
 
 extern void blas_cgemm(char transa, char transb, long M, long N, long K, const complex float alpha, long lda, const complex float A[M][lda], long ldb, const complex float B[K][ldb], const complex float beta, long ldc, complex float C[M][ldc]);
 extern void blas_csyrk(char uplow, char trans, long N, long K, complex float alpha, long lda, const complex float A[*][lda], complex float beta, long ldc, complex float C[*][ldc]);
+
+extern void blas_cgemv(char trans, long M, long N, complex float alpha, long lda, const complex float A[N][lda], long incx, const complex float* x, complex float beta, long incy, complex float* y);
+extern void blas_gemv_zfmac(long M, long N, complex float* y, const complex float* A, char trans, const complex float* x);
+extern void blas_sgemv(char trans, long M, long N, float alpha, long lda, const float A[N][lda], long incx, const float* x, float beta, long incy, float* y);
+extern void blas_gemv_fmac(long M, long N, float* y, const float* A, char trans, const float* x);
