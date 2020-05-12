6a5eef7318b6fa9bf1fbf54077e15d0b461b4e40
xwang <xiaoqing.wang@med.uni-goettingen.de>
2020-05-12 05:56:12 +0200

    moba.mk: reduce the image size for a more efficient test

--- tests/moba.mk
+++ tests/moba.mk
@@ -75,23 +75,23 @@ tests/test-moba-t1-no-IR: phantom signal fft ones index scale moba looklocker fm
 
 tests/test-moba-t1-nonCartesian: traj transpose phantom signal nufft fft ones index scale moba looklocker resize fmac nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
-	$(TOOLDIR)/traj -x32 -y1 -r -D -G -s7 -t300 _traj.ra  		                ;\
+	$(TOOLDIR)/traj -x16 -y1 -r -D -G -s7 -t300 _traj.ra  		                ;\
 	$(TOOLDIR)/transpose 5 10 _traj.ra _traj2.ra    		                ;\
 	$(TOOLDIR)/scale 0.5 _traj2.ra traj.ra   	    		                ;\
 	$(TOOLDIR)/phantom -k -c -t traj.ra basis_geom.ra    	    		        ;\
 	$(TOOLDIR)/signal -F -I -r0.005 -n300 -1 1.12:1.12:1 -2 100:100:1 signal.ra	;\
 	$(TOOLDIR)/fmac -s 64 basis_geom.ra signal.ra data.ra				;\
- 	$(TOOLDIR)/ones 16 1 32 1 1 1 300 1 1 1 1 1 1 1 1 1 1 ones.ra	   		;\
-	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra ones.ra pattern.ra	   		;\
+ 	$(TOOLDIR)/ones 16 1 16 1 1 1 300 1 1 1 1 1 1 1 1 1 1 ones.ra	   		;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a _traj2.ra ones.ra pattern.ra	   		;\
 	$(TOOLDIR)/fft -u 3 pattern.ra psf.ra				   		;\
-	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra data.ra zerofill_reco.ra  		;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a _traj2.ra data.ra zerofill_reco.ra  		;\
 	$(TOOLDIR)/fft -u 3 zerofill_reco.ra k_space.ra					;\
 	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
 	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                    	       		;\
 	$(TOOLDIR)/moba -L -l1 -i11 -C200 -j0.01 -p psf.ra k_space.ra TI.ra reco.ra	;\
 	$(TOOLDIR)/looklocker -t0.1 -D0. reco.ra T1.ra		    			;\
-	$(TOOLDIR)/resize -c 0 16 1 16 T1.ra T1_crop.ra					;\
-	$(TOOLDIR)/phantom -x16 -c circ.ra						;\
+	$(TOOLDIR)/resize -c 0 8 1 8 T1.ra T1_crop.ra					;\
+	$(TOOLDIR)/phantom -x8 -c circ.ra						;\
 	$(TOOLDIR)/fmac T1_crop.ra circ.ra masked.ra	    				;\
 	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra			    			;\
 	$(TOOLDIR)/nrmse -t 0.02 masked.ra ref.ra			    		;\
@@ -100,16 +100,16 @@ tests/test-moba-t1-nonCartesian: traj transpose phantom signal nufft fft ones in
 
 tests/test-moba-t1-nufft: traj transpose phantom signal nufft fft ones index scale moba fmac nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
-	$(TOOLDIR)/traj -x32 -y1 -r -D -G -s7 -t300 _traj.ra  		                ;\
+	$(TOOLDIR)/traj -x16 -y1 -r -D -G -s7 -t300 _traj.ra  		                ;\
 	$(TOOLDIR)/transpose 5 10 _traj.ra _traj2.ra    		                ;\
 	$(TOOLDIR)/scale 0.5 _traj2.ra traj.ra   	    		                ;\
 	$(TOOLDIR)/phantom -k -c -t traj.ra basis_geom.ra    	    		        ;\
 	$(TOOLDIR)/signal -F -I -r0.005 -n300 -1 1.12:1.12:1 -2 100:100:1 signal.ra	;\
 	$(TOOLDIR)/fmac -s 64 basis_geom.ra signal.ra data.ra				;\
- 	$(TOOLDIR)/ones 16 1 32 1 1 1 300 1 1 1 1 1 1 1 1 1 1 ones.ra	   		;\
-	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra ones.ra pattern.ra	   		;\
+ 	$(TOOLDIR)/ones 16 1 16 1 1 1 300 1 1 1 1 1 1 1 1 1 1 ones.ra	   		;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a _traj2.ra ones.ra pattern.ra	   		;\
 	$(TOOLDIR)/fft -u 3 pattern.ra psf.ra				   		;\
-	$(TOOLDIR)/nufft -d 32:32:1 -a _traj2.ra data.ra zerofill_reco.ra  		;\
+	$(TOOLDIR)/nufft -d 16:16:1 -a _traj2.ra data.ra zerofill_reco.ra  		;\
 	$(TOOLDIR)/fft -u 3 zerofill_reco.ra k_space.ra					;\
 	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
 	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                    	       		;\
@@ -121,11 +121,11 @@ tests/test-moba-t1-nufft: traj transpose phantom signal nufft fft ones index sca
 
 tests/test-moba-t1-MOLLI: phantom signal fft ones index scale moba slice invert fmac nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
-	$(TOOLDIR)/phantom -x32 -c circ.ra 		                  		;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra 		                  		;\
 	$(TOOLDIR)/signal -M -I -r0.004 -n300 -1 1.12:1.12:1 -2 100:100:1 -t0.36 -b5 signal.ra ;\
 	$(TOOLDIR)/fmac circ.ra signal.ra image.ra					;\
 	$(TOOLDIR)/fft 3 image.ra k_space.ra						;\
-	$(TOOLDIR)/ones 6 32 32 1 1 1 300 psf.ra					;\
+	$(TOOLDIR)/ones 6 16 16 1 1 1 300 psf.ra					;\
 	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
 	$(TOOLDIR)/scale 0.004 tmp1.ra TI.ra            	       			;\
 	$(TOOLDIR)/ones 1 4 tmp2.ra   					                ;\
@@ -133,7 +133,7 @@ tests/test-moba-t1-MOLLI: phantom signal fft ones index scale moba slice invert 
 	$(TOOLDIR)/moba -L -m -i11 -f1 -C200 -T t1relax.ra -p psf.ra k_space.ra TI.ra reco.ra ;\
 	$(TOOLDIR)/slice 6 1 reco.ra R1.ra						;\
 	$(TOOLDIR)/invert R1.ra T1.ra							;\
-	$(TOOLDIR)/phantom -x32 -c circ.ra						;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra						;\
 	$(TOOLDIR)/fmac T1.ra circ.ra masked.ra						;\
 	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra						;\
 	$(TOOLDIR)/nrmse -t 0.00007 masked.ra ref.ra					;\
@@ -142,11 +142,11 @@ tests/test-moba-t1-MOLLI: phantom signal fft ones index scale moba slice invert 
 
 tests/test-moba-t1-irgnm-admm: phantom signal fmac fft ones index scale moba looklocker nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
-	$(TOOLDIR)/phantom -x32 -c circ.ra 		                  		;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra 		                  		;\
 	$(TOOLDIR)/signal -I -F -r0.005 -n600 -1 1.12:1.12:1 -2 100:100:1 signal.ra     ;\
 	$(TOOLDIR)/fmac circ.ra signal.ra image.ra					;\
 	$(TOOLDIR)/fft 3 image.ra k_space.ra						;\
-	$(TOOLDIR)/ones 6 32 32 1 1 1 600 psf.ra					;\
+	$(TOOLDIR)/ones 6 16 16 1 1 1 600 psf.ra					;\
 	$(TOOLDIR)/index 5 600 tmp1.ra   						;\
 	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                  	       			;\
 	$(TOOLDIR)/moba -L -l3 -i11 -f1 -p psf.ra k_space.ra TI.ra reco.ra	        ;\
