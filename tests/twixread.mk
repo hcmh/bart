1c49cebd5733ca6960badbc4cfbac0d67674767b
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-05-25 17:23:43 +0200

    Update data paths in twixread test.

--- tests/twixread.mk
+++ tests/twixread.mk
@@ -1,9 +1,9 @@
 
-TWIXFILE1=/home/ague/archive/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
+TWIXFILE1=/home/ague/archive/pha/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
 TWIXFILE2=/home/ague/data/T1825/T1825.dat
-TWIXFILE3=/home/ague/archive/2018/2018-04-13_RT-Export/U00000013.dat
-TWIXFILE4=/home/ague/archive/2019/2019-03-25_Other-MU_Export/meas_MID00311_FID65562_t1_fl2d.dat
-TWIXFILE5=/home/ague/archive/vol/2020-02-27-MRT5_MyT1_0038/meas_MID00044_FID160057_UE02_UMG_Radial_ssl_fully_mulpIR.dat
+TWIXFILE3=/home/ague/archive/pha/2018/2018-04-13_RT-Export/U00000013.dat
+TWIXFILE4=/home/ague/archive/pha/2019/2019-03-25_Other-MU_Export/meas_MID00311_FID65562_t1_fl2d.dat
+TWIXFILE5=/home/ague/archive/vol/2020-02-27_MRT5_MyT1_0038/meas_MID00044_FID160057_UE02_UMG_Radial_ssl_fully_mulpIR.dat
 
 tests/test-twixread: twixread ${TWIXFILE1}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
