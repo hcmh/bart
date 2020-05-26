e730974ae1575610cb4b3db8c0749d4b03a4eb93
mblum <moritz.blumenthal@med.uni-goettingen.de>
2020-05-26 16:46:44 +0200

    Add tests

--- utests/test_convcorr.c
+++ utests/test_convcorr.c
@@ -25,6 +25,42 @@ static bool test_convcorr_frw_cf(void)
 }
 UT_REGISTER_TEST(test_convcorr_frw_cf);
 
+static bool test_convcorr_frw_cf_2d(void)
+{
+	enum {N = 6};
+	long odims[N] = {2, 1, 4, 3, 1, 4};
+	long idims[N] = {1, 4, 6, 7, 1, 4};
+	long kdims[N] = {2, 4, 3, 5, 1, 1};
+
+	bool test = test_zconvcorr_fwd(	N,
+					odims, MD_STRIDES(N, odims, CFL_SIZE),
+					idims, MD_STRIDES(N, idims, CFL_SIZE),
+					kdims, MD_STRIDES(N, kdims, CFL_SIZE),
+					28, NULL, NULL, false,
+					1.e-6, 2, 2);
+
+	UT_ASSERT(test);
+}
+UT_REGISTER_TEST(test_convcorr_frw_cf_2d);
+
+static bool test_convcorr_bwd_in_cf_2d(void)
+{
+	enum {N = 6};
+	long odims[N] = {2, 1, 4, 3, 1, 4};
+	long idims[N] = {1, 4, 6, 7, 1, 4};
+	long kdims[N] = {2, 4, 3, 5, 1, 1};
+
+	bool test = test_zconvcorr_bwd_in(	N,
+					odims, MD_STRIDES(N, odims, CFL_SIZE),
+					idims, MD_STRIDES(N, idims, CFL_SIZE),
+					kdims, MD_STRIDES(N, kdims, CFL_SIZE),
+					28, NULL, NULL, false,
+					1.e-6, 1, 2);
+
+	UT_ASSERT(test);
+}
+UT_REGISTER_TEST(test_convcorr_bwd_in_cf_2d);
+
 static bool test_convcorr_bwd_in_cf(void)
 {
 	enum {N = 6};
@@ -46,9 +82,9 @@ UT_REGISTER_TEST(test_convcorr_bwd_in_cf);
 static bool test_convcorr_bwd_krn_cf(void)
 {
 	enum {N = 6};
-	long odims[N] = {2, 1, 3, 2, 1, 4};
+	long odims[N] = {1, 1, 3, 2, 1, 4};
 	long idims[N] = {1, 5, 5, 5, 4, 4};
-	long kdims[N] = {2, 5, 3, 4, 4, 1};
+	long kdims[N] = {1, 5, 3, 4, 4, 1};
 
 	bool test = test_zconvcorr_bwd_krn(	N,
 					odims, MD_STRIDES(N, odims, CFL_SIZE),
@@ -60,3 +96,21 @@ static bool test_convcorr_bwd_krn_cf(void)
 	UT_ASSERT(test);
 }
 UT_REGISTER_TEST(test_convcorr_bwd_krn_cf);
+
+static bool test_convcorr_bwd_krn_cf_2d(void)
+{
+	enum {N = 6};
+	long odims[N] = {1, 1, 3, 2, 1, 4};
+	long idims[N] = {1, 5, 5, 5, 1, 4};
+	long kdims[N] = {1, 5, 3, 4, 1, 1};
+
+	bool test = test_zconvcorr_bwd_krn(	N,
+					odims, MD_STRIDES(N, odims, CFL_SIZE),
+					idims, MD_STRIDES(N, idims, CFL_SIZE),
+					kdims, MD_STRIDES(N, kdims, CFL_SIZE),
+					28, NULL, NULL, false,
+					1.e-6, 2, 2);
+
+	UT_ASSERT(test);
+}
+UT_REGISTER_TEST(test_convcorr_bwd_krn_cf_2d);
