f16262648ee8561a3d863600835e990dcbe61b4c
mblum <moritz.blumenthal@med.uni-goettingen.de>
2020-05-26 13:38:07 +0200

    Add iPALM algorithm

--- src/iter/iter6.c
+++ src/iter/iter6.c
@@ -4,6 +4,7 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
 
 #include "num/ops.h"
 #include "num/multind.h"
@@ -17,13 +18,13 @@
 
 #include "iter/italgos.h"
 #include "iter/vec.h"
+#include "iter/iter2.h"
 #include "iter/iter6_ops.h"
+
+#include "misc/mmio.h"
 
 #include "iter6.h"
 
-#ifndef STRUCT_TMP_COPY
-#define STRUCT_TMP_COPY(x) ({ __typeof(x) __foo = (x); __typeof(__foo)* __foo2 = alloca(sizeof(__foo)); *__foo2 = __foo; __foo2; })
-#endif
 #define NLOP2ITNLOP(nlop) (struct iter_nlop_s){ (NULL == nlop) ? NULL : iter6_nlop, CAST_UP(STRUCT_TMP_COPY(((struct iter6_nlop_s){ { &TYPEID(iter6_nlop_s) }, nlop }))) }
 #define NLOP2IT_ADJ_ARR(nlop) ({\
 	long NO = nlop_get_nr_out_args(nlop);\
@@ -38,6 +39,7 @@
 
 DEF_TYPEID(iter6_sgd_conf);
 DEF_TYPEID(iter6_adadelta_conf);
+DEF_TYPEID(iter6_iPALM_conf);
 
 
 const struct iter6_sgd_conf iter6_sgd_conf_defaults = {
@@ -65,6 +67,34 @@ const struct iter6_adadelta_conf iter6_adadelta_conf_defaults = {
 	.clip_val = 0.0,
 
 	.rho = 0.95
+};
+
+
+const struct iter6_iPALM_conf iter6_iPALM_conf_defaults = {
+
+	.INTERFACE.TYPEID = &TYPEID2(iter6_iPALM_conf),
+
+	.L = 1000.,
+	.Lmin = 1.e-4,
+	.Lmax = 1.e12,
+	.Lshrink = 2.,
+	.Lincrease = 9.,
+
+	.alpha = -1.,
+	.beta = -1.,
+	.convex = false,
+
+	.trivial_stepsize = false,
+
+	.alpha_arr = NULL,
+	.beta_arr =NULL,
+	.convex_arr = NULL,
+
+	.epochs = 1000,
+
+	.save_modulo = 100,
+	.save_path = NULL,
+	.save_name = NULL,
 };
 
 
@@ -133,7 +163,7 @@ static void iter6_op_arr_fun_deradj(iter_op_data* _o, int NO, unsigned long ofla
 #endif
 }
 
-static void iter6_op_arr_fun_update(iter_op_data* _o, int NO, unsigned long oflags, float* dst[NO], int NI, unsigned long iflags, const float* src[NI])
+static void iter6_op_arr_fun_diag(iter_op_data* _o, int NO, unsigned long oflags, float* dst[NO], int NI, unsigned long iflags, const float* src[NI])
 {
 	const struct iter6_op_arr_s* data = CAST_DOWN(iter6_op_arr_s, _o);
 
@@ -177,7 +207,7 @@ void iter6_adadelta(iter6_conf* _conf,
 		}
 	}
 	struct iter6_op_arr_s upd_ops_data = { { &TYPEID(iter6_op_arr_s) }, NI, NI, &(upd_ops[0][0])};
-	struct iter_op_arr_s upd_op_arr ={iter6_op_arr_fun_update, CAST_UP(&upd_ops_data)};
+	struct iter_op_arr_s upd_op_arr ={iter6_op_arr_fun_diag, CAST_UP(&upd_ops_data)};
 
 
 	for (int i = 0; i < NI; i++)
@@ -197,4 +227,160 @@ void iter6_adadelta(iter6_conf* _conf,
 
 	for (int i = 0; i < NI; i++)
 		operator_free(upd_ops[i][i]);
+}
+
+
+void iter6_iPALM(	iter6_conf* _conf,
+			const struct nlop_s* nlop,
+			long NI, enum IN_TYPE in_type[NI], float* dst[NI],
+			long NO, enum OUT_TYPE out_type[NO],
+			const struct operator_p_s* prox_ops[NI],
+			const struct nlop_s* nlop_batch_gen)
+{
+	auto conf = CAST_DOWN(iter6_iPALM_conf, _conf);
+
+	//Compute sizes
+	long isize[NI];
+	long osize[NO];
+	for (int i = 0; i < NI; i++)
+		isize[i] = 2 * md_calc_size(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims);
+	for (int o = 0; o < NO; o++)
+		osize[o] = 2 * md_calc_size(nlop_generic_codomain(nlop, o)->N, nlop_generic_codomain(nlop, o)->dims);
+
+	//create iter operators
+	struct iter_nlop_s nlop_iter = NLOP2ITNLOP(nlop);
+	struct iter_op_arr_s adj_op_arr = NLOP2IT_ADJ_ARR(nlop);
+	struct iter_nlop_s nlop_batch_gen_iter = NLOP2ITNLOP(nlop_batch_gen);
+
+	struct iter_op_p_s prox_iter[NI];
+	for (unsigned int i = 0; i < NI; i++)
+		prox_iter[i] = OPERATOR_P2ITOP(prox_ops[i]);
+
+	//compute parameter arrays
+	float alpha[NI];
+	float beta[NI];
+	bool convex[NI];
+
+	for (int i = 0; i < NI; i++) {
+
+		alpha[i] = (NULL == conf->alpha_arr) ? conf->alpha : conf->alpha_arr[i];
+		beta[i] = (NULL == conf->beta_arr) ? conf->beta : conf->beta_arr[i];
+		convex[i] = (NULL == conf->convex_arr) ? conf->convex : conf->convex_arr[i]; 
+	}
+
+	//gpu ref (dst[i] can be null if batch_gen)
+	float* gpu_ref = NULL;
+	for (int i = 0; i < NI; i++)
+		if (IN_OPTIMIZE == in_type[i])
+			gpu_ref = dst[i];
+	assert(NULL != gpu_ref);
+
+	float* x_old[NI];
+	for (int i = 0; i < NI; i++)
+		if (IN_OPTIMIZE == in_type[i])
+			x_old[i] = md_alloc_sameplace(1, isize + i, FL_SIZE, gpu_ref);
+		else
+			x_old[i] = NULL;
+
+	int epoch_start = conf->epoch_start;
+	int epoch_end = MIN(conf->epochs, epoch_start + conf->save_modulo);
+
+	float lipshitz_constants[NI];
+	for (int i = 0; i < NI; i++)
+		lipshitz_constants[i] = conf->L;
+
+	if (0 != epoch_start) {
+
+		if ((NULL != conf->save_path) && (NULL != conf->save_name)) {
+			for (int i = 0; i < NI; i++) {
+
+				if(!((IN_OPTIMIZE == in_type[i]) && (NULL != conf->save_name[i])))
+					continue;
+
+				long dims[nlop_generic_domain(nlop, i)->N];
+				char filename[strlen(conf->save_path) + strlen(conf->save_name[i]) + 50];
+					
+				sprintf(filename, "%s/%s-%d", conf->save_path, conf->save_name[i], epoch_start);
+				complex float* file = load_cfl(filename, nlop_generic_domain(nlop, i)->N, dims);
+				md_copy(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, (complex float*)dst[i], file, CFL_SIZE);						unmap_cfl(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file);
+
+				sprintf(filename, "%s/%s-%d", conf->save_path, conf->save_name[i], epoch_start - 1);
+				file = load_cfl(filename, nlop_generic_domain(nlop, i)->N, dims);
+				md_copy(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, (complex float*)x_old[i], file, CFL_SIZE);
+				unmap_cfl(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file);
+			}
+		} else {
+
+			error("Could not load data from epoch %d\n", epoch_start);
+		}
+
+		long dims[1];
+		char filename[strlen(conf->save_path) + 70];
+		sprintf(filename, "%s/%s-%d", conf->save_path, "lipshitz_constants", epoch_start);
+		complex float* file = load_cfl(filename, 1, dims);
+		md_copy2(1, dims, MAKE_ARRAY((long)FL_SIZE), lipshitz_constants, MAKE_ARRAY((long)CFL_SIZE), file, FL_SIZE);
+		unmap_cfl(1, dims, file);
+	}
+
+
+	while (epoch_start < epoch_end) {
+
+		if ((NULL != conf->save_path) && (NULL != conf->save_name)) {
+
+			for (int i = 0; i < NI; i++)
+				if((IN_OPTIMIZE == in_type[i]) && (NULL != conf->save_name[i])) {
+
+					char filename[strlen(conf->save_path) + strlen(conf->save_name[i]) + 50];
+					sprintf(filename, "%s/%s-%d", conf->save_path, conf->save_name[i], epoch_start);
+
+					complex float* file = create_cfl(filename, nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims);
+					md_copy(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file, (complex float*)dst[i], CFL_SIZE);
+					unmap_cfl(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file);
+
+					//save old weights (for warm start)
+					if (0 < epoch_start) {
+
+						sprintf(filename, "%s/%s-%d", conf->save_path, conf->save_name[i], epoch_start - 1);
+						file = create_cfl(filename, nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims);
+						md_copy(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file, (complex float*)x_old[i], CFL_SIZE);
+						unmap_cfl(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file);
+					}
+				}
+
+			char filename[strlen(conf->save_path) + 50];
+			sprintf(filename, "%s/%s-%d", conf->save_path, "lipshitz_constants", epoch_start);
+			complex float* file = create_cfl(filename, 1, MAKE_ARRAY(NI));
+			md_copy2(1, MAKE_ARRAY(NI), MAKE_ARRAY((long)CFL_SIZE), file, MAKE_ARRAY((long)FL_SIZE), lipshitz_constants, FL_SIZE);
+			unmap_cfl(1, MAKE_ARRAY(NI), file);
+		}
+
+		iPALM(	NI, isize, in_type, dst, x_old,
+			NO, osize, out_type,
+			epoch_start, epoch_end,
+        		select_vecops(gpu_ref),
+			alpha, beta, convex, conf->trivial_stepsize,
+			lipshitz_constants, conf->Lmin, conf->Lmax, conf->Lshrink, conf->Lincrease,
+        		nlop_iter, adj_op_arr,
+			prox_iter,
+			nlop_batch_gen_iter,
+			(struct iter_op_s){ NULL, NULL }, NULL);
+		
+		epoch_start = epoch_end;
+		epoch_end = MIN(conf->epochs, epoch_start + conf->save_modulo);
+	}
+
+	for (int i = 0; i < NI; i++) {
+		if((NULL != conf->save_path) && (NULL != conf->save_name) && (IN_OPTIMIZE == in_type[i]) && (NULL != conf->save_name[i])) {
+
+			char filename[strlen(conf->save_path) + strlen(conf->save_name[i]) + 50];
+			sprintf(filename, "%s/%s-%d", conf->save_path, conf->save_name[i], epoch_end);
+					
+			complex float* file = create_cfl(filename, nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims);
+			md_copy(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file, (complex float*)dst[i], CFL_SIZE);
+			unmap_cfl(nlop_generic_domain(nlop, i)->N, nlop_generic_domain(nlop, i)->dims, file);
+		}
+
+		if(IN_OPTIMIZE == in_type[i])
+			md_free(x_old[i]);
+	}
 }
--- src/iter/iter6.h
+++ src/iter/iter6.h
@@ -1,4 +1,8 @@
+#ifndef ITER6_H
+#define ITER6_H
+
 #include "italgos.h"
+
 
 typedef struct iter6_conf_s { TYPEID* TYPEID; } iter6_conf;
 struct iter_op_s;
@@ -29,13 +33,53 @@ struct iter6_adadelta_conf {
 	float rho;
 };
 
+struct iter6_iPALM_conf {
+
+	INTERFACE(iter6_conf);
+
+	float L;
+	float Lmin;
+	float Lmax;
+	float Lshrink;
+	float Lincrease;
+
+	float alpha;
+	float beta;
+	_Bool convex;
+
+	_Bool trivial_stepsize;
+
+	float* alpha_arr;
+	float* beta_arr;
+	_Bool* convex_arr;
+
+	int epochs;
+	int epoch_start;
+
+	int save_modulo;
+	char* save_path;
+	char** save_name;
+};
+
 extern const struct iter6_sgd_conf iter6_sgd_conf_defaults;
 extern const struct iter6_adadelta_conf iter6_adadelta_conf_defaults;
+extern const struct iter6_iPALM_conf iter6_iPALM_conf_defaults;
 
 struct iter3_conf_s;
 struct iter_nlop_s;
 struct nlop_s;
+struct operator_p_s;
 
 typedef void iter6_f(iter6_conf* _conf, const struct nlop_s* nlop, long NI, enum IN_TYPE in_type[NI], float* dst[NI], long NO, enum OUT_TYPE out_type[NO], int N_batch, int N_total);
 
 iter6_f iter6_adadelta;
+
+
+void iter6_iPALM(	iter6_conf* _conf,
+			const struct nlop_s* nlop,
+			long NI, enum IN_TYPE in_type[__VLA(NI)], float* dst[__VLA(NI)],
+			long NO, enum OUT_TYPE out_type[__VLA(NO)],
+			const struct operator_p_s* prox_ops[__VLA(NI)],
+			const struct nlop_s* nlop_batch_gen);
+
+#endif
