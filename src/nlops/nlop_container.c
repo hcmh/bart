8853953bcd571512f07bc71b25ba343b3d183a8d
mblum <moritz.blumenthalqmed.uni-goettingen.de>
2020-06-20 08:36:34 +0200

    Fix warnings

--- src/nlops/nlop_container.c
+++ src/nlops/nlop_container.c
@@ -89,8 +89,8 @@ const struct nlop_s* nlop_container(const struct nlop_s* op)
 	}
 
 	operator_prop_flags_t props[II][OO];
-	for (unsigned int i = 0; i < II; i++)
-		for (unsigned int o = 0; o < OO; o++)
+	for (int i = 0; i < II; i++)
+		for (int o = 0; o < OO; o++)
 			props[i][o] = operator_get_prop_flags_oi(op->op, o, i);
 
 	n->op = operator_generic_extopts_create2(A, operator_ioflags(op->op), D, op_dims, op_strs, CAST_UP(PTR_PASS(data)), nlop_container_apply, nlop_container_free, props);
