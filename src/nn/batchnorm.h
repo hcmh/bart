4a8e569200072d791a38048f468ccf4258937a53
mblum <moritz.blumenthla@med.uni-goettingen.de>
2020-06-23 18:22:09 +0200

    Guard header

--- src/nn/batchnorm.h
+++ src/nn/batchnorm.h
@@ -1,5 +1,8 @@
+#ifndef BATCHNORM_H
+#define BATCHNORM_H
 extern const struct nlop_s* nlop_stats_create(int N, const long dims[__VLA(N)], unsigned long flags);
 extern const struct nlop_s* nlop_normalize_create(int N, const long dims[__VLA(N)], unsigned long flags, float epsilon);
 extern const struct nlop_s* nlop_scale_and_shift_create(int N, const long dims[__VLA(N)], unsigned long flags);
 extern const struct nlop_s* nlop_batchnorm_floatingstats_create(int N, const long dims[__VLA(N)], unsigned long flags);
 extern const struct nlop_s* nlop_batchnorm_create(int N, const long dims[N], unsigned long flags, float epsilon);
+#endif
