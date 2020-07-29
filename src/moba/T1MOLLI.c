9e1eb610ecf220be981e95209a455ca465999d36
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2020-07-29 16:47:52 +0200

    moba: clear up MOLLI

--- src/moba/T1MOLLI.c
+++ src/moba/T1MOLLI.c
@@ -62,35 +62,24 @@ struct nlop_s* nlop_T1MOLLI_create(int N, const long map_dims[N], const long out
 	struct nlop_s* T1s_5 = nlop_T1srelax_create(N, map_dims, out_dims, TI_dims, TI2, use_gpu);
         
         // first chain: T1(T1s)
-        struct nlop_s* T1c_combine = nlop_combine(T1_1, T1s_1);
-        struct nlop_s* T1c_link = nlop_link(T1c_combine, 2, 0);   
-        struct nlop_s* T1c_dup1 = nlop_dup(T1c_link, 1, 4);
-        struct nlop_s* T1c_dup1_1 = nlop_dup(T1c_dup1, 0, 3);
-
-        nlop_free(T1s_1);
-        nlop_free(T1_1);
-        nlop_free(T1c_combine);
+        struct nlop_s* T1c_combine = nlop_combine_FF(T1_1, T1s_1);
+        struct nlop_s* T1c_link = nlop_link_F(T1c_combine, 2, 0);   
+        struct nlop_s* T1c_dup1 = nlop_dup_F(T1c_link, 1, 4);
+        struct nlop_s* T1c_dup1_1 = nlop_dup_F(T1c_dup1, 0, 3);
 
         // second chain T1s(T1(T1s))
-        struct nlop_s* T1c_combine2 = nlop_combine(T1s_2, T1c_dup1_1);
-        struct nlop_s* T1c_link2 = nlop_link(T1c_combine2, 2, 0);
-        struct nlop_s* T1c_dup2 = nlop_dup(T1c_link2, 2, 6);
-        struct nlop_s* T1c_dup2_1 = nlop_dup(T1c_dup2, 1, 4);
-        struct nlop_s* T1c_dup2_2 = nlop_dup(T1c_dup2_1, 0, 3);
+        struct nlop_s* T1c_combine2 = nlop_combine_FF(T1s_2, T1c_dup1_1);
+        struct nlop_s* T1c_link2 = nlop_link_F(T1c_combine2, 2, 0);
+        struct nlop_s* T1c_dup2 = nlop_dup_F(T1c_link2, 2, 6);
+        struct nlop_s* T1c_dup2_1 = nlop_dup_F(T1c_dup2, 1, 4);
+        struct nlop_s* T1c_dup2_2 = nlop_dup_F(T1c_dup2_1, 0, 3);
 
-        nlop_free(T1c_link);
-        nlop_free(T1s_2);
-        nlop_free(T1c_combine2);
 
         // third chain: T1(T1s(T1(T1s)))
-        struct nlop_s* T1c_combine3 = nlop_combine(T1_2, T1c_dup2_2);
-        struct nlop_s* T1c_link3 = nlop_link(T1c_combine3, 2, 0);
-        struct nlop_s* T1c_dup3 = nlop_dup(T1c_link3, 1, 3);
-        struct nlop_s* T1c_dup3_1 = nlop_dup(T1c_dup3, 0, 2);
-
-        nlop_free(T1c_combine3);
-        nlop_free(T1_2);
-        nlop_free(T1c_dup2_2);
+        struct nlop_s* T1c_combine3 = nlop_combine_FF(T1_2, T1c_dup2_2);
+        struct nlop_s* T1c_link3 = nlop_link_F(T1c_combine3, 2, 0);
+        struct nlop_s* T1c_dup3 = nlop_dup_F(T1c_link3, 1, 3);
+        struct nlop_s* T1c_dup3_1 = nlop_dup_F(T1c_dup3, 0, 2);
 
         //  stack the outputs together
         long sodims[N];
@@ -98,111 +87,71 @@ struct nlop_s* nlop_T1MOLLI_create(int N, const long map_dims[N], const long out
         sodims[TE_DIM] = 2 * out_dims[TE_DIM];
 
         struct nlop_s* stack = nlop_stack_create(N, sodims, out_dims, out_dims, TE_DIM);
-        struct nlop_s* T1c_combine_stack = nlop_combine(stack, T1c_dup3_1);
-        struct nlop_s* T1c_link_stack_1 = nlop_link(T1c_combine_stack, 3, 0);
-        struct nlop_s* T1c_link_stack_2 = nlop_link(T1c_link_stack_1, 2, 0);
-
-        nlop_free(stack);
-        nlop_free(T1c_combine_stack);
-        nlop_free(T1c_dup3_1);
-        nlop_free(T1c_link_stack_1); 
+        struct nlop_s* T1c_combine_stack = nlop_combine_FF(stack, T1c_dup3_1);
+        struct nlop_s* T1c_link_stack_1 = nlop_link_F(T1c_combine_stack, 3, 0);
+        struct nlop_s* T1c_link_stack_2 = nlop_link_F(T1c_link_stack_1, 2, 0);
 
         // fourth chain : T1s(T1(T1s(T1(T1s))))
-        struct nlop_s* T1c_combine4 = nlop_combine(T1s_3, T1c_link_stack_2);
-        struct nlop_s* T1c_link4 = nlop_link(T1c_combine4, 3, 0);
-        struct nlop_s* T1c_dup4 = nlop_dup(T1c_link4, 2, 5);
-        struct nlop_s* T1c_dup4_1 = nlop_dup(T1c_dup4, 1, 4);
-        struct nlop_s* T1c_dup4_2 = nlop_dup(T1c_dup4_1, 0, 3);
-
-        nlop_free(T1c_combine4);
-        nlop_free(T1s_3);
-        nlop_free(T1c_link_stack_2);
+        struct nlop_s* T1c_combine4 = nlop_combine_FF(T1s_3, T1c_link_stack_2);
+        struct nlop_s* T1c_link4 = nlop_link_F(T1c_combine4, 3, 0);
+        struct nlop_s* T1c_dup4 = nlop_dup_F(T1c_link4, 2, 5);
+        struct nlop_s* T1c_dup4_1 = nlop_dup_F(T1c_dup4, 1, 4);
+        struct nlop_s* T1c_dup4_2 = nlop_dup_F(T1c_dup4_1, 0, 3);
 
         //  stack the outputs together
         long sodims1[N];
         md_copy_dims(N, sodims1, out_dims);
         sodims1[TE_DIM] = sodims[TE_DIM] + out_dims[TE_DIM];
         struct nlop_s* stack1 = nlop_stack_create(N, sodims1, sodims, out_dims, TE_DIM);
-        struct nlop_s* T1c_combine_stack1 = nlop_combine(stack1, T1c_dup4_2);
-        struct nlop_s* T1c_link_stack_1_1 = nlop_link(T1c_combine_stack1, 3, 0);
-        struct nlop_s* T1c_link_stack_2_1 = nlop_link(T1c_link_stack_1_1, 1, 0);
+        struct nlop_s* T1c_combine_stack1 = nlop_combine_FF(stack1, T1c_dup4_2);
+        struct nlop_s* T1c_link_stack_1_1 = nlop_link_F(T1c_combine_stack1, 3, 0);
+        struct nlop_s* T1c_link_stack_2_1 = nlop_link_F(T1c_link_stack_1_1, 1, 0);
 
         
-        nlop_free(stack1);
-        nlop_free(T1c_combine_stack1);
-        nlop_free(T1c_dup4_2);
-        nlop_free(T1c_link_stack_1_1); 
-
         // fifth chain : T1(T1s(T1(T1s(T1(T1s)))))
-        struct nlop_s* T1c_combine5 = nlop_combine(T1_3, T1c_link_stack_2_1);
-        struct nlop_s* T1c_link5 = nlop_link(T1c_combine5, 2, 0);
-        struct nlop_s* T1c_dup5 = nlop_dup(T1c_link5, 1, 3);
-        struct nlop_s* T1c_dup5_1 = nlop_dup(T1c_dup5, 0, 2);
-
-        nlop_free(T1c_combine5);
-        nlop_free(T1_3);
-        nlop_free(T1c_link_stack_2_1);
+        struct nlop_s* T1c_combine5 = nlop_combine_FF(T1_3, T1c_link_stack_2_1);
+        struct nlop_s* T1c_link5 = nlop_link_F(T1c_combine5, 2, 0);
+        struct nlop_s* T1c_dup5 = nlop_dup_F(T1c_link5, 1, 3);
+        struct nlop_s* T1c_dup5_1 = nlop_dup_F(T1c_dup5, 0, 2);
 
         // Sixth chain : T1s(T1(T1s(T1(T1s(T1(T1s))))))
-        struct nlop_s* T1c_combine6 = nlop_combine(T1s_4, T1c_dup5_1);
-        struct nlop_s* T1c_link6 = nlop_link(T1c_combine6, 2, 0);
-        struct nlop_s* T1c_dup6 = nlop_dup(T1c_link6, 2, 5);
-        struct nlop_s* T1c_dup6_1 = nlop_dup(T1c_dup6, 1, 4);
-        struct nlop_s* T1c_dup6_2 = nlop_dup(T1c_dup6_1, 0, 3);
+        struct nlop_s* T1c_combine6 = nlop_combine_FF(T1s_4, T1c_dup5_1);
+        struct nlop_s* T1c_link6 = nlop_link_F(T1c_combine6, 2, 0);
+        struct nlop_s* T1c_dup6 = nlop_dup_F(T1c_link6, 2, 5);
+        struct nlop_s* T1c_dup6_1 = nlop_dup_F(T1c_dup6, 1, 4);
+        struct nlop_s* T1c_dup6_2 = nlop_dup_F(T1c_dup6_1, 0, 3);
 
-        nlop_free(T1c_combine6);
-        nlop_free(T1s_4);
-        nlop_free(T1c_dup5_1);
-
-        //  stack the outputs together
+         //  stack the outputs together
         long sodims2[N];
         md_copy_dims(N, sodims2, out_dims);
         sodims2[TE_DIM] = sodims1[TE_DIM] + out_dims[TE_DIM];
         struct nlop_s* stack2 = nlop_stack_create(N, sodims2, sodims1, out_dims, TE_DIM);
-        struct nlop_s* T1c_combine_stack2 = nlop_combine(stack2, T1c_dup6_2);
-        struct nlop_s* T1c_link_stack_1_2 = nlop_link(T1c_combine_stack2, 3, 0);
-        struct nlop_s* T1c_link_stack_2_2 = nlop_link(T1c_link_stack_1_2, 1, 0);
-
-        nlop_free(stack2);
-        nlop_free(T1c_combine_stack2);
-        nlop_free(T1c_dup6_2);
-        nlop_free(T1c_link_stack_1_2); 
-
+        struct nlop_s* T1c_combine_stack2 = nlop_combine_FF(stack2, T1c_dup6_2);
+        struct nlop_s* T1c_link_stack_1_2 = nlop_link_F(T1c_combine_stack2, 3, 0);
+        struct nlop_s* T1c_link_stack_2_2 = nlop_link_F(T1c_link_stack_1_2, 1, 0);
 
         // seventh chain : T1(T1s(T1(T1s(T1(T1s(T1(T1s)))))))
-        struct nlop_s* T1c_combine7 = nlop_combine(T1_4, T1c_link_stack_2_2);
-        struct nlop_s* T1c_link7 = nlop_link(T1c_combine7, 2, 0);
-        struct nlop_s* T1c_dup7 = nlop_dup(T1c_link7, 1, 3);
-        struct nlop_s* T1c_dup7_1 = nlop_dup(T1c_dup7, 0, 2);
-
-        nlop_free(T1c_combine7);
-        nlop_free(T1_4);
-        nlop_free(T1c_link_stack_2_2);
+        struct nlop_s* T1c_combine7 = nlop_combine_FF(T1_4, T1c_link_stack_2_2);
+        struct nlop_s* T1c_link7 = nlop_link_F(T1c_combine7, 2, 0);
+        struct nlop_s* T1c_dup7 = nlop_dup_F(T1c_link7, 1, 3);
+        struct nlop_s* T1c_dup7_1 = nlop_dup_F(T1c_dup7, 0, 2);
 
         // eighth chain : T1s(T1(T1s(T1(T1s(T1(T1s(T1(T1s))))))))
-        struct nlop_s* T1c_combine8 = nlop_combine(T1s_5, T1c_dup7_1);
-        struct nlop_s* T1c_link8 = nlop_link(T1c_combine8, 2, 0);
-        struct nlop_s* T1c_dup8 = nlop_dup(T1c_link8, 2, 5);
-        struct nlop_s* T1c_dup8_1 = nlop_dup(T1c_dup8, 1, 4);
-        struct nlop_s* T1c_dup8_2 = nlop_dup(T1c_dup8_1, 0, 3);
+        struct nlop_s* T1c_combine8 = nlop_combine_FF(T1s_5, T1c_dup7_1);
+        struct nlop_s* T1c_link8 = nlop_link_F(T1c_combine8, 2, 0);
+        struct nlop_s* T1c_dup8 = nlop_dup_F(T1c_link8, 2, 5);
+        struct nlop_s* T1c_dup8_1 = nlop_dup_F(T1c_dup8, 1, 4);
+        struct nlop_s* T1c_dup8_2 = nlop_dup_F(T1c_dup8_1, 0, 3);
 
-        nlop_free(T1c_combine8);
-        nlop_free(T1s_5);
-        nlop_free(T1c_dup7_1);
 
         //  stack the outputs together
         long sodims3[N];
         md_copy_dims(N, sodims3, out_dims);
         sodims3[TE_DIM] = sodims2[TE_DIM] + out_dims[TE_DIM];
         struct nlop_s* stack3 = nlop_stack_create(N, sodims3, sodims2, out_dims, TE_DIM);
-        struct nlop_s* T1c_combine_stack3 = nlop_combine(stack3, T1c_dup8_2);
-        struct nlop_s* T1c_link_stack_1_3 = nlop_link(T1c_combine_stack3, 3, 0);
-        struct nlop_s* T1c_link_stack_2_3 = nlop_link(T1c_link_stack_1_3, 1, 0);
-
-        nlop_free(stack3);
-        nlop_free(T1c_combine_stack3);
-        nlop_free(T1c_dup8_2);
-        nlop_free(T1c_link_stack_1_3); 
+        struct nlop_s* T1c_combine_stack3 = nlop_combine_FF(stack3, T1c_dup8_2);
+        struct nlop_s* T1c_link_stack_1_3 = nlop_link_F(T1c_combine_stack3, 3, 0);
+        struct nlop_s* T1c_link_stack_2_3 = nlop_link_F(T1c_link_stack_1_3, 1, 0);
 
         struct nlop_s* T1c_del = nlop_del_out(T1c_link_stack_2_3, 1);
 
@@ -216,13 +165,10 @@ struct nlop_s* nlop_T1MOLLI_create(int N, const long map_dims[N], const long out
 
         linop_free(linop_scalar);
 
-        struct nlop_s* T1c_combine_scale = nlop_combine(T1c_del, nl_scalar);
-        struct nlop_s* T1c_link_scale = nlop_link(T1c_combine_scale, 1, 3);
-        struct nlop_s* T1c_dup_scale = nlop_dup(T1c_link_scale, 0, 3);
+        struct nlop_s* T1c_combine_scale = nlop_combine_FF(T1c_del, nl_scalar);
+        struct nlop_s* T1c_link_scale = nlop_link_F(T1c_combine_scale, 1, 3);
+        struct nlop_s* T1c_dup_scale = nlop_dup_F(T1c_link_scale, 0, 3);
 
-        nlop_free(nl_scalar);
-        nlop_free(T1c_del);
-        nlop_free(T1c_combine_scale);
 
         md_free(scale);
 
