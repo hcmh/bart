603a58d3ca6f798f7ad28d66c0fa1bfc53b8570d
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingne.de>
2020-08-20 10:56:32 +0200

    Add chained operators with fixed memory leak

--- src/num/ops.c
+++ src/num/ops.c
@@ -1950,11 +1950,17 @@ static const struct operator_chain_s* get_chain_data(const struct operator_s* ch
  */
 const struct operator_s* operator_chain(const struct operator_s* a, const struct operator_s* b)
 {
-	#if 0
+	#if 1
 	if (NULL != get_plus_data(b)) {
 
 		auto bd = get_plus_data(b);
-		return operator_plus_create(operator_chain(a, bd->a), operator_chain(a, bd->b));
+		auto tmpa = operator_chain(a, bd->a);
+		auto tmpb = operator_chain(a, bd->b);
+
+		auto result = operator_plus_create(tmpa, tmpb);
+		operator_free(tmpa);
+		operator_free(tmpb);
+		return result;
 	}
 	#endif
 
