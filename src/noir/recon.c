19882381100d36706248a6fd1c939e32404af2e9
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-09-01 13:34:12 +0200

    formatting

--- src/noir/recon.c
+++ src/noir/recon.c
@@ -134,12 +134,13 @@ void noir_recon(const struct noir_conf_s* conf, const long dims[DIMS], complex f
 
 		if (conf->img_space_coils) { // transform coils back to k-space
 
-				complex float* coil_tmp = md_alloc(DIMS, coil_dims, CFL_SIZE);
-				ifftmod(DIMS, coil_dims, mconf.fft_flags, coil_tmp, ref + skip);
-				noir_back_coils(nl.linop, coil_tmp, coil_tmp);
-				md_copy(DIMS, imgs_dims, xref, ref, CFL_SIZE);
-				md_copy(DIMS, coil_dims, xref + skip, coil_tmp, CFL_SIZE);
-				md_free(coil_tmp);
+			complex float* coil_tmp = md_alloc(DIMS, coil_dims, CFL_SIZE);
+			ifftmod(DIMS, coil_dims, mconf.fft_flags, coil_tmp, ref + skip);
+			noir_back_coils(nl.linop, coil_tmp, coil_tmp);
+			md_copy(DIMS, imgs_dims, xref, ref, CFL_SIZE);
+			md_copy(DIMS, coil_dims, xref + skip, coil_tmp, CFL_SIZE);
+			md_free(coil_tmp);
+
 		} else {
 
 			md_copy(1, d1, xref, ref, CFL_SIZE);
