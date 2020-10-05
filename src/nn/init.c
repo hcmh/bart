cd589608b397115abf73925784cf71d94abc93a9
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2020-10-05 21:20:58 +0200

    Refactor initializers

--- src/nn/init.c
+++ src/nn/init.c
@@ -7,73 +7,114 @@
 #include "num/multind.h"
 #include "num/flpmath.h"
 #include "num/rand.h"
+
+#include "nn/initializer.h"
 #include "init.h"
 
-static float get_dense_init_scaling(const long weight_dims[2])
-{
-	return (float)sqrt(6. / (double)(weight_dims[0] + weight_dims[0]));
-}
 
-static float get_conv_init_scaling(const long kernel_dims[5], bool channel_first)
-{
-	//filters, channel, kx, ky, kz    or x, y, z channel, filters
-	int in_neurons = channel_first ? kernel_dims[1] : kernel_dims[3];
-	int out_neurons = channel_first ? kernel_dims[0] : kernel_dims[4];
-
-	for (int i = 0; i < 3; i++){
-
-		in_neurons *= kernel_dims[i + (channel_first ? 0 : 2)];
-		out_neurons *= kernel_dims[i + (channel_first ? 0 : 2)];
-	}
-
-	return (float)sqrt(6. / (double)(in_neurons + out_neurons));
-}
-
-
+/**
+ * Initializes weights using glorot/xavier methode, i.e. uniform distribution with std=sqrt(2/(n_in+n_out))
+ * Note that only the real part is initialized
+ */
 complex float* init_glorot_uniform_dense(long N, const long* weight_dims, complex float* src, _Bool c1)
 {
 	UNUSED(c1);
 
-	long size = md_calc_size(N, weight_dims);
-	md_uniform_rand(1, &size, src);
-	md_zsadd(1, &size, src, src, (complex float)(-0.5));
-	md_zreal(1, &size, src, src);
-	md_zsmul(1, &size, src, src, (complex float)(2 * get_dense_init_scaling(weight_dims)));
-	return src + size;
+	const struct initializer_s* init = init_xavier_create(MD_BIT(1), MD_BIT(0), true, true);
+	initializer_apply(init, N, weight_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, weight_dims);
 }
 
+
+/**
+ * Initializes weights using glorot/xavier methode, i.e. uniform distribution with std=sqrt(2/(n_in+n_out))
+ * Note that only the real part is initialized
+ */
 complex float* init_glorot_uniform_conv(long N, const long* kernel_dims, complex float* src, _Bool c1)
 {
-	long size = md_calc_size(N, kernel_dims);
-	md_uniform_rand(1, &size, src);
-	md_zsadd(1, &size, src, src, (complex float)(-0.5));
-	md_zreal(1, &size, src, src);
-	md_zsmul(1, &size, src, src, (complex float)(2 * get_conv_init_scaling(kernel_dims, c1)));
-	return src + size;
+
+	const struct initializer_s* init = init_xavier_create(in_flag_conv(c1), out_flag_conv(c1), true, true);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
 }
 
+/**
+ * Initializes weights using glorot/xavier methode, i.e. uniform distribution with std=sqrt(2/(n_in+n_out))
+ * Note that the variance is understood as variance of complex random variables
+ */
 complex float* init_glorot_uniform_conv_complex(long N, const long* kernel_dims, complex float* src, _Bool c1)
 {
-	long size = md_calc_size(N, kernel_dims);
-	md_uniform_rand(1, &size, src);
-	md_zsadd(1, &size, src, src, (complex float)(-0.5));
-	complex float* tmp = md_alloc_sameplace(1, &size, CFL_SIZE, src);
-	md_uniform_rand(1, &size, tmp);
-	md_zsadd(1, &size, tmp, tmp, (complex float)(-0.5));
-	md_zaxpy(1, &size, src, 1.I, tmp);
-	md_free(tmp);
-	md_zsmul(1, &size, src, src, (complex float)(2 * get_conv_init_scaling(kernel_dims, c1)));
-	return src + size;
+	const struct initializer_s* init = init_xavier_create(in_flag_conv(c1), out_flag_conv(c1), false, true);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
+}
+
+/**
+ * Initializes weights using kaiming methode, i.e. uniform distribution with std=sqrt(2/(n_in * (1 + leaky_val**2)))
+ * Note that the variance is understood as variance of complex random variables
+ */
+complex float* init_kaiming_uniform_conv(long N, const long* kernel_dims, complex float* src, _Bool c1, float leaky_val)
+{
+	const struct initializer_s* init = init_kaiming_create(in_flag_conv(c1), true, true, leaky_val);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
+}
+/**
+ * Initializes weights using kaiming methode, i.e. uniform distribution with std=sqrt(2/(n_in * (1 + leaky_val**2)))
+ * Note that the variance is understood as variance of complex random variables
+ */
+complex float* init_kaiming_uniform_conv_complex(long N, const long* kernel_dims, complex float* src, _Bool c1, float leaky_val)
+{
+	const struct initializer_s* init = init_kaiming_create(in_flag_conv(c1), false, true, leaky_val);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
+}
+
+/**
+ * Initializes weights using kaiming methode, i.e. uniform distribution with std=sqrt(2/(n_in * (1 + leaky_val**2)))
+ * Note that the variance is understood as variance of complex random variables
+ */
+complex float* init_kaiming_normal_conv_complex(long N, const long* kernel_dims, complex float* src, _Bool c1, float leaky_val)
+{
+	const struct initializer_s* init = init_kaiming_create(in_flag_conv(c1), false, false, leaky_val);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
+}
+
+/**
+ * Initializes weights using kaiming methode, i.e. uniform distribution with std=sqrt(2/(n_in * (1 + leaky_val**2)))
+ * Note that the variance is understood as variance of complex random variables
+ */
+complex float* init_kaiming_normal_conv(long N, const long* kernel_dims, complex float* src, _Bool c1, float leaky_val)
+{
+	const struct initializer_s* init = init_kaiming_create(in_flag_conv(c1), true, false, leaky_val);
+	initializer_apply(init, N, kernel_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, kernel_dims);
 }
 
 complex float* init_bias(long N, const long* bias_dims, complex float* src, _Bool c1)
 {
 	UNUSED(c1);
-	UNUSED(N);
 
-	long size = bias_dims[0];
-	md_clear(1, &size, src, CFL_SIZE);
-	return src + size;
+	const struct initializer_s* init = init_const_create(0.);
+	initializer_apply(init, N, bias_dims, src);
+	initializer_free(init);
+
+	return src + md_calc_size(N, bias_dims);
 }
 /**
  * Initialize weights with initializer guessed from dimensions
