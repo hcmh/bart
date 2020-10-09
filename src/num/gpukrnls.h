6e5835cb28cb3a086e63643f7897e4ae2eadf086
Volkert Roeloffs <volkert.roeloffs@med.uni-goettingen.de>
2020-10-09 11:01:31 +0200

    add gpu kernels for zsin zcos

--- src/num/gpukrnls.h
+++ src/num/gpukrnls.h
@@ -36,6 +36,8 @@ extern void cuda_zconj(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zphsr(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zexpj(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zexp(long N, _Complex float* dst, const _Complex float* src);
+extern void cuda_zsin(long N, _Complex float* dst, const _Complex float* src);
+extern void cuda_zcos(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zlog(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zarg(long N, _Complex float* dst, const _Complex float* src);
 extern void cuda_zabs(long N, _Complex float* dst, const _Complex float* src);
