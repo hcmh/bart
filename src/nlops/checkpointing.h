2eda1b9a107a000f491a9734e5a6999d4e4f43a7
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2020-10-21 02:31:54 +0200

    Add checkpoints for automatic differentiation

---
+++ src/nlops/checkpointing.h
@@ -0,0 +1,2 @@
+extern const struct nlop_s* nlop_checkpoint_create(const struct nlop_s* nlop, _Bool der_once);
+extern const struct nlop_s* nlop_checkpoint_create_F(const struct nlop_s* nlop, _Bool der_once);
