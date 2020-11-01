hcmh/bart

commit dd0e5e845a54dc983c45fd1fa2c39edf7bee6559

traj MEMS: swap even echoes with base angle

src/traj.c
@@ -254,13 +254,6 @@ int main_traj(int argc, char* argv[])
 			 */
 			double read = (float)(i + D - X) + (conf.asym_traj ? 0 : 0.5) - (float)D / 2.;
 
-			if (conf.mems_traj) {
-				if (e % 2) // even
-					read = (float)(D - i - even_echo_shift) + (conf.asym_traj ? 0 : 0.5) - (float)D / 2.;
-				else // odd
-					read = (float)(i + D - X) + (conf.asym_traj ? 0 : 0.5) - (float)D / 2.;
-			}
-
 			if (conf.golden_partition) {
 
 				double golden_ratio = (sqrtf(5.) + 1.) / 2;

src/noncart/traj.c
@@ -180,7 +180,7 @@ void calc_base_angles(double base_angle[DIMS], int Y, int E, int mb, int turns, 
 	if (conf.mems_traj) {
 
 		angle_s = angle_s * 1.;
-		angle_e = angle_s / E;
+		angle_e = angle_s / E + M_PI;
 		angle_t = golden_angle;
 
 	} else if (conf.golden) {

commit c0969acd8d38639a6fa682e429fe1774dc811774

traj: fix asymmetric multi-echo
This fixes the problem with asymmetric MEMS trajectories introduced in
dd0e5e845a54dc983c45fd1fa2c39edf7bee6559

src/traj.c
@@ -254,6 +254,11 @@ int main_traj(int argc, char* argv[])
 			 */
 			double read = (float)(i + D - X) + (conf.asym_traj ? 0 : 0.5) - (float)D / 2.;
 
+			// For odd echos in asymmetric multi-echo, the DC component is later than half of the readout instead of earlier.
+			// Therefore, for such echos, the readout position needs to be calculated as follows:
+			if ((D != X) && (1 == (e % 2)))
+					read = (float)i + (conf.asym_traj ? 0. : 0.5) - (float)D / 2.;
+
 			if (conf.golden_partition) {
 
 				double golden_ratio = (sqrt(5.) + 1.) / 2;