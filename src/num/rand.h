44cc0fc3f23a53d9e4e29e2b27f48226b255105a
rosenzweig <sebastian.rosenzweig@med.uni-goettingen.de>
2020-11-02 16:41:54 +0100

    num/rand: Generic random functions

--- src/num/rand.h
+++ src/num/rand.h
@@ -4,6 +4,9 @@
  */
 
 #include "misc/cppwrap.h"
+#include <stdint.h>
+
+#define RAND_MAX_SPCG32 4294967295
 
 extern double uniform_rand(void);
 extern _Complex double gaussian_rand(void);
@@ -11,5 +14,9 @@ extern void md_gaussian_rand(unsigned int D, const long dims[__VLA(D)], _Complex
 extern void md_uniform_rand(unsigned int D, const long dims[__VLA(D)], _Complex float* dst);
 extern void md_rand_one(unsigned int D, const long dims[__VLA(D)], _Complex float* dst, double p);
 extern void num_rand_init(unsigned int seed);
+extern uint32_t rand_spcg32(uint64_t s[1]);
+extern _Complex double rand_spcg32_normal(uint64_t rand_seed[1]);
+
+
 
 #include "misc/cppwrap.h"
