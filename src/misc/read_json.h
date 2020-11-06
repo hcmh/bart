8138cdf1ab80633de95f9ae7129a3df30b2a2eef
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2020-11-06 20:32:53 +0100

    Add functions to simply read json

--- src/misc/read_json.h
+++ src/misc/read_json.h
@@ -1,4 +1,5 @@
 
+typedef struct cJSON cJSON;
 #include "simu/shepplogan.h"
 
 extern char* readfile(const char *filename);
@@ -6,3 +7,15 @@ extern char* readfile(const char *filename);
 extern void read_json_to_struct(int N, struct ellipsis_s phantom[N], const cJSON* json_object);
 
 extern void check_json_file(cJSON *json_file);
+
+extern _Bool cJSON_not_NULL(const cJSON* object, const char* key);
+extern int cJSON_get_int(const cJSON* object, const char* key);
+extern float cJSON_get_float(const cJSON* object, const char* key);
+extern _Bool cJSON_get_bool(const cJSON* object, const char* key);
+
+extern void cJSON_set_long(long* dst, const cJSON* object, const char* key);
+extern void cJSON_set_uint(unsigned int* dst, const cJSON* object, const char* key);
+extern void cJSON_set_ulong(unsigned long* dst, const cJSON* object, const char* key);
+extern void cJSON_set_int(int* dst, const cJSON* object, const char* key);
+extern void cJSON_set_bool(_Bool* dst, const cJSON* object, const char* key);
+extern void cJSON_set_float(float* dst, const cJSON* object, const char* key);
