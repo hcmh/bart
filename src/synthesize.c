a9c11120497b910a3311ddae12c40065015d92e3
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2020-11-06 15:00:28 +0100

    src/synthesize.c: fix the time unit and some tabSize issue

--- src/synthesize.c
+++ src/synthesize.c
@@ -41,48 +41,48 @@ int main_synthesize(int argc, char* argv[argc])
         float scaling_R2 = 10.0;
 
         enum seq_type { IR_FLASH, TSE};
-	enum seq_type seq = IR_FLASH;
+        enum seq_type seq = IR_FLASH;
 
-	const struct opt_s opts[] = {
+        const struct opt_s opts[] = {
 
                 OPT_SELECT('F', enum seq_type, &seq, IR_FLASH, "IR_FLASH"),
-		OPT_SELECT('T', enum seq_type, &seq, TSE, "TSE"),
-		OPT_FLOAT('R', &TR, "TR", "Repetition time (ms)"),
-                OPT_FLOAT('E', &TE, "TE", "Echo time (ms)"),
+                OPT_SELECT('T', enum seq_type, &seq, TSE, "TSE"),
+                OPT_FLOAT('R', &TR, "TR", "Repetition time (second)"),
+                OPT_FLOAT('E', &TE, "TE", "Echo time (second)"),
                 OPT_INT('s', &nspoke, "nspoke", "radial spokes per frame"),
                 OPT_INT('n', &nframe, "nframe", "number of contrast images"),
                 OPT_FLOAT('M', &scaling_M0, "scaling_M0", "scaling of M0 for IR FLASH sequence"),
                 OPT_FLOAT('S', &scaling_R2, "scaling_R2", "scaling of R2 for TSE sequence"),
 	};
 
-	cmdline(&argc, argv, 2, 2, usage_str, help_str, ARRAY_SIZE(opts), opts);
+        cmdline(&argc, argv, 2, 2, usage_str, help_str, ARRAY_SIZE(opts), opts);
 
-	num_init();
+        num_init();
 
-	long idims[DIMS];
-	complex float* in_data = load_cfl(argv[1], DIMS, idims);
+        long idims[DIMS];
+        complex float* in_data = load_cfl(argv[1], DIMS, idims);
 
-	long odims[DIMS];
-	md_select_dims(DIMS, ~COEFF_FLAG, odims, idims);
+        long odims[DIMS];
+        md_select_dims(DIMS, ~COEFF_FLAG, odims, idims);
         odims[TE_DIM] = nframe;
         
-	complex float* out_data = create_cfl(argv[2], DIMS, odims);
+        complex float* out_data = create_cfl(argv[2], DIMS, odims);
 
         long dims1[DIMS];
-	md_select_dims(DIMS, TE_FLAG, dims1, odims);
+        md_select_dims(DIMS, TE_FLAG, dims1, odims);
 
-	long istrs[DIMS];
-	md_calc_strides(DIMS, istrs, idims, CFL_SIZE);
+        long istrs[DIMS];
+        md_calc_strides(DIMS, istrs, idims, CFL_SIZE);
 
-	long ostrs[DIMS];
-	md_calc_strides(DIMS, ostrs, odims, CFL_SIZE);
+        long ostrs[DIMS];
+        md_calc_strides(DIMS, ostrs, odims, CFL_SIZE);
 
         long strs1[DIMS];
-	md_calc_strides(DIMS, strs1, dims1, CFL_SIZE);
+        md_calc_strides(DIMS, strs1, dims1, CFL_SIZE);
 
-	long pos[DIMS] = { 0 };
+        long pos[DIMS] = { 0 };
 
-	do {    
+        do {    
                 complex float out[nframe];
                 complex float M0;
 
@@ -97,7 +97,7 @@ int main_synthesize(int argc, char* argv[argc])
                                 complex float R1s = MD_ACCESS(DIMS, istrs, (pos[COEFF_DIM] = 2, pos), in_data); 
 
                                 for (int ind = 0; ind < nframe; ind++)
-		                        out[ind] = Ms - (Ms + scaling_M0 * M0) * exp(-TR * ind * nspoke * cabs(R1s));
+                                        out[ind] = Ms - (Ms + scaling_M0 * M0) * exp(-TR * ind * nspoke * cabs(R1s));
 
                                 break;
 
@@ -109,11 +109,13 @@ int main_synthesize(int argc, char* argv[argc])
                                 complex float R2 = MD_ACCESS(DIMS, istrs, (pos[COEFF_DIM] = 1, pos), in_data);
 
                                 for (int ind = 0; ind < nframe; ind++)
-		                        out[ind] = M0 * exp(-TE * ind * scaling_R2 * cabs(R2));
+                                        out[ind] = M0 * exp(-TE * ind * scaling_R2 * cabs(R2));
 
                                 break;
 
-                        default: error("sequence type not supported");
+                        default: 
+                                
+                                error("sequence type not supported");
                 }
 
                 pos[COEFF_DIM] = 0;
@@ -121,9 +123,9 @@ int main_synthesize(int argc, char* argv[argc])
 
 	} while(md_next(DIMS, odims, ~TE_FLAG, pos));
 
-	unmap_cfl(DIMS, idims, in_data);
-	unmap_cfl(DIMS, odims, out_data);
+        unmap_cfl(DIMS, idims, in_data);
+        unmap_cfl(DIMS, odims, out_data);
 
-	return 0;
+        return 0;
 }
 
