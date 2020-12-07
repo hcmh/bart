c83e5d2ff7a1c1e53ae569dc1f4f89e9c2a1f470
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2020-12-07 22:38:19 +0100

    Adjust sim.c syntax in mdb tests

--- tests/mdb.mk
+++ tests/mdb.mk
@@ -2,7 +2,7 @@
 tests/test-mdb-bloch: phantom sim fmac fft ones modbloch slice scale nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
 	$(TOOLDIR)/phantom -x16 -c basis_geom.ra				;\
-	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
+	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0:0.00001 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
 	$(TOOLDIR)/fmac basis_geom.ra basis_simu.ra image.ra		;\
 	$(TOOLDIR)/fft 3 image.ra k_space.ra					;\
 	$(TOOLDIR)/ones 6 16 16 1 1 1 1000 psf.ra		;\
@@ -23,7 +23,7 @@ tests/test-mdb-bloch-psf: traj repmat phantom sim fmac ones nufft fft modbloch s
 	$(TOOLDIR)/traj -x16 -y16 _traj.ra			;\
 	$(TOOLDIR)/repmat 5 1000 _traj.ra traj.ra	;\
 	$(TOOLDIR)/phantom -c -k -t traj.ra basis_geom.ra				;\
-	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
+	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0:0.00001 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
 	$(TOOLDIR)/fmac basis_geom.ra basis_simu.ra k_space.ra		;\
 	$(TOOLDIR)/ones 6 1 16 16 1 1 1000 ones.ra		;\
 	$(TOOLDIR)/nufft -d 16:16:1 -a traj.ra ones.ra psf.ra	;\
@@ -48,7 +48,7 @@ tests/test-mdb-bloch-traj: traj repmat phantom sim fmac modbloch slice scale nrm
 	$(TOOLDIR)/traj -x16 -y16 _traj.ra			;\
 	$(TOOLDIR)/repmat 5 1000 _traj.ra traj.ra	;\
 	$(TOOLDIR)/phantom -c -k -t traj.ra basis_geom.ra				;\
-	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
+	$(TOOLDIR)/sim -P 1:1:0.0045:0.00225:0.001:45:1000:0:0.00001 -1 1.12:1.12:1 -2 0.1:0.1:1 basis_simu.ra	;\
 	$(TOOLDIR)/fmac basis_geom.ra basis_simu.ra k_space.ra		;\
 	$(TOOLDIR)/modbloch -P 1:0.0045:0.00225:45:0.00001:0.00001:0.00001 -R2 -f1 -o0 -l1 -n0 -i15 -a1 -m0 -r0 -t traj.ra k_space.ra reco.ra sens.ra	;\
 	$(TOOLDIR)/slice 6 0 reco.ra t1map.ra				;\
