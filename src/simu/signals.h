73957204f6596046f9940394292201ecac50f5e1
Zhengguo Tan <zhengguo.tan@med.uni-goettingen.de>
2020-12-30 13:10:39 +0100

    dixon in signals

--- src/simu/signals.h
+++ src/simu/signals.h
@@ -45,6 +45,8 @@ extern void IR_bSSFP_model(const struct signal_model* data, int N, complex float
 
 extern const struct signal_model signal_multi_grad_echo_defaults;
 
+extern const struct signal_model signal_multi_grad_echo_dixon_defaults;
+
 extern complex float calc_fat_modulation(float b0, float TE);
 
 extern void multi_grad_echo_model(const struct signal_model* data, int N, complex float out[N]);
