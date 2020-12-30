f9b523acf0d40d406347848af6b1efd90cbf9cd4
Zhengguo Tan <zhengguo.tan@med.uni-goettingen.de>
2020-12-30 13:11:38 +0100

    update test cases

--- tests/mobafit.mk
+++ tests/mobafit.mk
@@ -1,8 +1,9 @@
 
-tests/test-mobafit-T2: phantom signal fmac index scale extract mobafit slice nrmse
+tests/test-mobafit-r2s: phantom signal fmac index scale extract mobafit slice nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
 	$(TOOLDIR)/phantom -x16 -c circ.ra				;\
-	$(TOOLDIR)/signal -G -n7 -1 3:3:1 -2 0.02:0.02:1 signal.ra	;\
+	$(TOOLDIR)/signal -G -n8 -1 3:3:1 -2 0.02:0.02:1 signal_p1.ra	;\
+	$(TOOLDIR)/extract 5 1 8 signal_p1.ra signal.ra			;\
 	$(TOOLDIR)/fmac circ.ra signal.ra echoes.ra			;\
 	$(TOOLDIR)/index 5 8 tmp1.ra					;\
 	$(TOOLDIR)/scale 1.6 tmp1.ra tmp2.ra				;\
@@ -12,8 +13,37 @@ tests/test-mobafit-T2: phantom signal fmac index scale extract mobafit slice nrm
 	$(TOOLDIR)/phantom -x16 -c circ.ra				;\
 	$(TOOLDIR)/fmac R2S.ra circ.ra masked.ra			;\
 	$(TOOLDIR)/scale -- 0.05 circ.ra ref.ra				;\
-	$(TOOLDIR)/nrmse -t 0.000001 ref.ra masked.ra			;\
+	$(TOOLDIR)/nrmse -t 0.000003 ref.ra masked.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-TESTS += tests/test-mobafit-T2
+tests/test-mobafit-wfr2s: phantom signal fmac index scale extract mobafit fatfrac slice nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra				;\
+	$(TOOLDIR)/signal -G -D -n8 -1 3:3:1 -2 0.02:0.02:1 signal_p1.ra	;\
+	$(TOOLDIR)/extract 5 1 8 signal_p1.ra signal.ra			;\
+	$(TOOLDIR)/fmac circ.ra signal.ra echoes.ra			;\
+	$(TOOLDIR)/index 5 8 tmp1.ra					;\
+	$(TOOLDIR)/scale 1.6 tmp1.ra tmp2.ra				;\
+	$(TOOLDIR)/extract 5 1 8 tmp2.ra TE.ra				;\
+	$(TOOLDIR)/mobafit -G -m1 TE.ra echoes.ra reco.ra		;\
+	$(TOOLDIR)/slice 6 0 reco.ra W.ra				;\
+	$(TOOLDIR)/slice 6 1 reco.ra F.ra				;\
+	$(TOOLDIR)/slice 6 2 reco.ra R2S.ra				;\
+	$(TOOLDIR)/slice 6 3 reco.ra fB0.ra				;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra				;\
+	$(TOOLDIR)/fatfrac W.ra F.ra fatfrac.ra			;\
+	$(TOOLDIR)/fmac fatfrac.ra circ.ra fatfrac_masked.ra			;\
+	$(TOOLDIR)/scale -- 0.2 circ.ra fatfrac_ref.ra				;\
+	$(TOOLDIR)/nrmse -t 0.000003 fatfrac_ref.ra fatfrac_masked.ra			;\
+	$(TOOLDIR)/fmac R2S.ra circ.ra R2S_masked.ra			;\
+	$(TOOLDIR)/scale -- 0.05 circ.ra R2S_ref.ra				;\
+	$(TOOLDIR)/nrmse -t 0.000003 R2S_ref.ra R2S_masked.ra			;\
+	$(TOOLDIR)/fmac fB0.ra circ.ra fB0_masked.ra			;\
+	$(TOOLDIR)/scale -- 0.02 circ.ra fB0_ref.ra				;\
+	$(TOOLDIR)/nrmse -t 0.000003 fB0_ref.ra fB0_masked.ra			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+TESTS += tests/test-mobafit-r2s tests/test-mobafit-wfr2s
