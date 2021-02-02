84a6d1db1b91e2104b987e6317197f13fc3f88a2
Xiaoqing Wang <xiaoqing.wang@med.uni-goettingen.de>
2021-02-02 16:05:37 +0100

    add a test for mobafit of IR LL

--- tests/mobafit.mk
+++ tests/mobafit.mk
@@ -53,4 +53,21 @@ tests/test-mobafit-wfr2s: phantom signal fmac index scale extract mobafit saxpy 
 	touch $@
 
 
+tests/test-mobafit-t1: phantom signal fmac index scale extract mobafit looklocker nrmse 
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	               		 	;\
+	$(TOOLDIR)/phantom -x16 -c circ.ra 		                  		;\
+	$(TOOLDIR)/signal -I -F -r0.005 -n300 -1 1.12:1.12:1 -2 100:100:1 signal.ra	;\
+	$(TOOLDIR)/fmac circ.ra signal.ra image.ra					;\
+	$(TOOLDIR)/index 5 300 tmp1.ra   						;\
+	$(TOOLDIR)/scale 0.005 tmp1.ra TI.ra                    	       		;\
+	$(TOOLDIR)/mobafit -L -i12 TI.ra image.ra reco.ra				;\
+	$(TOOLDIR)/looklocker -t0.1 -D0. reco.ra T1.ra		    			;\
+	$(TOOLDIR)/fmac T1.ra circ.ra masked.ra		    				;\
+	$(TOOLDIR)/scale -- 1.12 circ.ra ref.ra			    			;\
+	$(TOOLDIR)/nrmse -t 0.0001 masked.ra ref.ra			    		;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
 TESTS += tests/test-mobafit-r2s tests/test-mobafit-wfr2s
+TESTS += tests/test-mobafit-t1
