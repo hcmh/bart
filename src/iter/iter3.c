c1a5e4fea3983a4b338b72408d5a47e5faed1965
Christian Holme <christian.holme@med.uni-goettingen.de>
2021-02-05 16:36:33 +0100

    make alpha_min_exp_decay default in iter3
    and not only in the higher-level moba.c

--- src/iter/iter3.c
+++ src/iter/iter3.c
@@ -38,7 +38,7 @@ const struct iter3_irgnm_conf iter3_irgnm_defaults = {
 	.cgiter = 100,
 	.cgtol = 0.1,
 
-	.alpha_min_exp_decay = false,
+	.alpha_min_exp_decay = true,
 	.nlinv_legacy = false,
 };
 
