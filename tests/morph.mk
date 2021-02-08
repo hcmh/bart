61ed066a0e8129f2f4a91acde3472ed6d04c67c5
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2021-02-08 17:43:12 +0100

    Add test for opening and closing

--- tests/morph.mk
+++ tests/morph.mk
@@ -8,6 +8,25 @@ tests/test-morph-dilation-erosion: phantom morph nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-morph-opening: phantom morph nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/phantom -x64 -g 2 ori.ra					;\
+	$(TOOLDIR)/morph -e -b 9 ori.ra tmp.ra					;\
+	$(TOOLDIR)/morph -d -b 9 tmp.ra rec.ra					;\
+	$(TOOLDIR)/morph -o -b 9 ori.ra rec2.ra					;\
+	$(TOOLDIR)/nrmse -t 0.000001 rec2.ra rec.ra 				;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
-TESTS += tests/test-morph-dilation-erosion
+tests/test-morph-closing: phantom morph nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/phantom -x64 -g 2 ori.ra					;\
+	$(TOOLDIR)/morph -d -b 9 ori.ra tmp.ra					;\
+	$(TOOLDIR)/morph -e -b 9 tmp.ra rec.ra					;\
+	$(TOOLDIR)/morph -c -b 9 ori.ra rec2.ra					;\
+	$(TOOLDIR)/nrmse -t 0.000001 rec2.ra rec.ra 				;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+TESTS += tests/test-morph-dilation-erosion tests/test-morph-opening tests/test-morph-closing
 
