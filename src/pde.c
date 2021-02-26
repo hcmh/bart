92a85232aef78921bc5e4f771a91e67fff0755ef
Philip Schaten <philip.schaten@med.uni-goettingen.de>
2021-02-26 17:08:27 +0100

    cleanup pde tool

--- src/pde.c
+++ src/pde.c
@@ -14,14 +14,53 @@
 
 #include "iter/lsqr.h"
 
+#include "iter/monitor.h"
+#include "misc/debug.h"
+
 #include "linops/linop.h"
 
 #include "misc/mmio.h"
 #include "misc/io.h"
 #include "misc/misc.h"
 #include "misc/opts.h"
+#include "misc/nested.h"
 
 #include <math.h>
+
+
+static void calc_j(const long N, const long d, const struct linop_s *d_op,
+			const long j_dims[N],  complex float* j,
+			const long   dims[N], const complex float* mask2,
+			const complex float* phi)
+{
+	long mask_strs[N], j_strs[N];
+	md_calc_strides(N, j_strs, j_dims, CFL_SIZE);
+	md_calc_strides(N, mask_strs, dims, CFL_SIZE);
+
+	linop_forward(d_op, N, j_dims, j, N, dims, phi); // minus phi, probably
+
+	mask_strs[d] = 0;
+	md_zmul2(N, j_dims, j_strs, j, mask_strs, mask2, j_strs, j);
+}
+
+
+struct process_dat {
+	complex float *j;
+	const long N;
+	const long *dims;
+	const long *j_dims;
+	complex float* mask2;
+	struct linop_s * op;
+};
+
+
+static complex float *j_wrapper(void *_data, const float* phi)
+{
+	auto data = (struct process_dat*)_data;
+	calc_j(data->N, data->N-1, data->op, data->j_dims, data->j, data->dims, data->mask2, (complex float*)phi);
+	return data->j;
+}
+
 
 static const char usage_str[] = "<boundary> <electrodes> <output_phi> <output_j>";
 static const char help_str[] = "Solve 3D Laplace equation with Neumann Boundary Conditions";
@@ -30,8 +69,10 @@ int main_pde(int argc, char* argv[])
 {
 	long N = 4;
 	int iter=100;
+	int hist = -1;
 	const struct opt_s opts[] = {
 		OPT_INT('n', &iter, "n", "Iterations"),
+		OPT_INT('p', &hist, "n", "Save ∇ɸ every n iterations"),
 	};
 
 	cmdline(&argc, argv, 4, 4, usage_str, help_str, ARRAY_SIZE(opts), opts);
@@ -42,10 +83,11 @@ int main_pde(int argc, char* argv[])
 	complex float* electrodes = load_cfl(argv[2], N, dims1);
 
 	assert(1 == dims[N-1]);
+
 	for(int i = 0; i < N; i++)
 		assert(dims[i] == dims1[i]);
 
-	//calculate normal
+	//Boundary Values
 	long grad_dim = N - 1;
 	long grad_dims[N];
 	md_copy_dims(N, grad_dims, dims);
@@ -54,12 +96,13 @@ int main_pde(int argc, char* argv[])
 	complex float *normal = md_alloc(N, grad_dims, CFL_SIZE);
 	calc_outward_normal(N, grad_dims, normal, grad_dim, dims, mask);
 
-	//calculate list of points on the border
 	const long boundary_dimensions[] =  { md_calc_size(N, dims) };
-
 	struct boundary_point_s *boundary = md_alloc(1, boundary_dimensions, sizeof(struct boundary_point_s));
+	long n_points = calc_boundary_points(N, grad_dims, boundary, grad_dim, normal, electrodes);
 
-	long n_points = calc_boundary_points(N, grad_dims, boundary, grad_dim, normal);
+	complex float *mask2 = md_calloc(N, dims, CFL_SIZE);
+	shrink_wrap(N, dims, mask2, n_points, boundary, mask);
+	//\Boundary Values//
 
 	//create laplace
 	struct sparse_diag_s *mat = sd_laplace_create(N - 1, dims);
@@ -74,27 +117,24 @@ int main_pde(int argc, char* argv[])
 
 	// RHS
 	complex float *rhs = md_calloc(1, vec_dim, CFL_SIZE);
-	long index_strides[N - 1], strs[N], zstrs[N], pos[N];
-	pos[grad_dim] = 1;
-	calc_index_strides(N - 1, index_strides, dims);
-	md_calc_strides(N, strs, dims, CFL_SIZE);
-	md_calc_strides(N, zstrs, dims, CFL_SIZE);
-	for (long i = 0; i < n_points; i++) {
-		for (int j = 0; j < N - 1; j++)
-			pos[j] = boundary[i].index[j] + boundary[i].dir[j];
-
-		long offset = md_calc_offset(N, zstrs, pos);
-		complex float val = *(complex float *)((void *)electrodes + offset);
-		boundary[i].val = val;
-		if ( cabsf(val) > 0 ) {
-			offset = md_calc_offset(N - 1, strs, boundary[i].index);
-			*(complex float *)((void *)rhs + offset) -= val;
-		}
-
-	}
+	set_laplace_neumann_rhs(N - 1, dims, rhs, n_points, boundary);
 
 	//phi
 	complex float *phi = md_calloc(1, vec_dim, CFL_SIZE);
+
+	// setup monitoring
+	complex float *j_hist = md_calloc(N, grad_dims, CFL_SIZE);
+	auto d_op = linop_fd_create(N, dims, grad_dim, 7, 2, BC_ZERO, false);
+
+	struct process_dat j_dat = { .N=N, .dims = dims, .j_dims = grad_dims, .mask2 = mask2, .j = j_hist, .op = d_op };
+
+	NESTED(bool, selector, (const unsigned long iter, const float *x, void *_data))
+	{
+		UNUSED(x);
+		UNUSED(_data);
+		return hist > 0 ? ( 0 == iter % 30 ) : false;
+	};
+	auto mon = create_monitor_recorder(N, grad_dims, "j_step", (void *)&j_dat, selector, j_wrapper);
 
 	//solve
 	struct iter_conjgrad_conf conf = iter_conjgrad_defaults;
@@ -102,46 +142,26 @@ int main_pde(int argc, char* argv[])
 	conf.l2lambda = 1e-5;
 
 	long size = 2 * md_calc_size(1, vec_dim); // multiply by 2 for float size
-	iter_conjgrad(CAST_UP(&conf), diff_op->forward, NULL, size, (float*)phi, (const float*)rhs, NULL);
+	iter_conjgrad(CAST_UP(&conf), diff_op->forward, NULL, size, (float*)phi, (const float*)rhs, mon);
 
+	//save
 	complex float* out = create_cfl(argv[3], N, dims);
-
 	md_copy(N, dims, out, phi, CFL_SIZE);
 
-
-	//homogenous conductivity - calc j, fill in neumann boundary infos
 	complex float* j_out = create_cfl(argv[4], N, grad_dims);
-	auto d_op = linop_fd_create(N, dims, grad_dim, 7, 2, BC_ZERO, false);
-	linop_forward(d_op, N, grad_dims, j_out, N, dims, phi); // minus phi, probably
-	linop_free(d_op);
+	calc_j(N, N-1, d_op, grad_dims, j_out, dims, mask2, phi);
 
-	pos[grad_dim] = 1;
-	calc_index_strides(N - 1, index_strides, dims);
-	md_calc_strides(N, strs,grad_dims, CFL_SIZE);
-
-	md_copy_dims(N, zstrs, strs);
-	zstrs[grad_dim] = 0;
-	md_zmul2(N, grad_dims, strs, j_out, zstrs, mask, strs, j_out);
-
-	for (long i = 0; i < n_points; i++) {
-		float ext_points = 0;
-		for (int j = 0; j < N - 1; j++) {
-			ext_points += pow(boundary[i].dir[j], 2);
-			pos[j] = boundary[i].index[j];
-		}
-		for (int j = 0; j < N - 1; j++) {
-			pos[grad_dim] = j;
-			long offset = md_calc_offset(N, strs, pos);
-			*(complex float *)((void *)j_out + offset) = -1 * boundary[i].dir[j] * boundary[i].val * pow(ext_points, .5);
-		}
-
-	}
-
+	//cleanup
 	md_free(normal);
 	md_free(boundary);
 	md_free(phi);
 	md_free(rhs);
+	md_free(mask2);
 	linop_free(diff_op);
+	linop_free(d_op);
+
+	md_free(j_hist);
+	xfree(mon);
 
 	unmap_cfl(N, dims, mask);
 	unmap_cfl(N, dims, electrodes);
