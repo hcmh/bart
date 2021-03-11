770a12b807bd2a895f59febe2eecd34cc04d06e7
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2021-03-11 16:26:12 +0100

    Add utest: test_bloch_irflash_frw_init_relax
    Taking initial relaxation effects into account.

--- utests/test_bloch_op.c
+++ utests/test_bloch_op.c
@@ -485,6 +485,123 @@ static bool test_bloch_irflash_adj_spoke_av(void)
 }
 UT_REGISTER_TEST(test_bloch_irflash_adj_spoke_av);
 
+static bool test_bloch_irflash_frw_init_relax(void)
+{
+	enum { N = 16 };
+	enum { rep = 300 };
+	long map_dims[N] = { 16, 16, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long out_dims[N] = { 16, 16, 1, 1, 1, rep, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long in_dims[N] = { 16, 16, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long all_dims[N] = { 16, 16, 1, 1, 1, rep, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long TI_dims[N] = { 1, 1, 1, 1, 1, rep, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+	bool gpu_use = false;
+
+	// Init and apply Bloch model operator
+
+	complex float* src = md_alloc(N, in_dims, CFL_SIZE);
+	md_zfill(N, in_dims, src, 1.0);
+
+	complex float* dst_frw_bloch = md_alloc(N, out_dims, CFL_SIZE);
+
+	struct modBlochFit fit_para = modBlochFit_defaults;
+
+	// IR FLASH characteristics
+	fit_para.sequence = 5;
+	fit_para.tr = 0.003;
+	fit_para.te = 0.001;
+	fit_para.fa = 8.;
+	fit_para.rfduration = 0.00001;
+	fit_para.inversion_pulse_length = 0.01;
+	fit_para.prep_pulse_length = 0.;
+	fit_para.look_locker_assumptions = true; // ! Analytical Assumption: Mxy(t=TE) == Mz(t=0)
+
+
+	struct nlop_s* Bloch = nlop_Bloch_create(N, all_dims, map_dims, out_dims, in_dims, &fit_para, gpu_use);
+
+	nlop_apply(Bloch, N, out_dims, dst_frw_bloch, N, in_dims, src);
+
+	// dump_cfl("_dst_frw_bloch", N, out_dims, dst_frw_bloch);
+
+	nlop_free(Bloch);
+	md_free(src);
+
+
+	// Init and apply IR FLASH model operator
+
+	in_dims[COEFF_DIM] = 2;
+
+	complex float* src2 = md_alloc(N, in_dims, CFL_SIZE);
+	md_zfill(N, in_dims, src2, 1.0);
+
+	complex float* dst_frw_irflash = md_alloc(N, out_dims, CFL_SIZE);
+
+	// Set parameter M0 and R1
+
+	// Correction for initial relaxation effects
+	// Deichmann, R. (2005),
+	// Fast high‐resolution T1 mapping of the human brain.
+	// Magn. Reson. Med., 54: 20-27.
+	// Equation 9 and 10
+	complex float initval[2] = {	-(1. - 2. * expf(-fit_para.inversion_pulse_length)),
+					1. / (1. - 2.*fit_para.inversion_pulse_length) };
+
+	long pos[DIMS] = { [0 ... DIMS - 1] = 0 };
+
+	complex float* tmp = md_alloc(DIMS, map_dims, CFL_SIZE);
+
+	for (int i = 0; i < in_dims[COEFF_DIM]; i++) {
+
+		pos[COEFF_DIM] = i;
+		md_zfill(N, map_dims, tmp, initval[i]);
+		md_copy_block(DIMS, pos, in_dims, src2, map_dims, tmp, CFL_SIZE);
+	}
+
+	md_free(tmp);
+
+	// Inversion times
+	complex float* TI = md_alloc(N, TI_dims, CFL_SIZE);
+
+	for (int i = 0; i < rep; i++)
+		TI[i] =  i * fit_para.tr;
+
+	// alpha map
+	complex float* fa = md_alloc(N, map_dims, CFL_SIZE);
+	md_zfill(N, map_dims, fa, fit_para.fa);
+
+	complex float* alpha = md_alloc(N, map_dims, CFL_SIZE);
+
+	fa_to_alpha(DIMS, map_dims, alpha, fa, fit_para.tr);
+
+	md_free(fa);
+
+	struct nlop_s* T1 = nlop_T1_alpha_in_create(N, map_dims, out_dims, in_dims, TI_dims, TI, alpha, gpu_use);
+
+	nlop_apply(T1, N, out_dims, dst_frw_irflash, N, in_dims, src2);
+
+	// dump_cfl("_dst_frw_irflash", N, out_dims, dst_frw_irflash);
+
+	nlop_free(T1);
+	md_free(src2);
+	md_free(TI);
+	md_free(alpha);
+
+	// Compare operator outputs
+
+	float err_frw = md_znrmse(N, out_dims, dst_frw_irflash, dst_frw_bloch);
+	// debug_printf(DP_INFO, "Error Forward: %f\n", err_frw);
+
+	md_free(dst_frw_bloch);
+	md_free(dst_frw_irflash);
+
+	if (err_frw > 3.5E-3)
+		return 0;
+
+	return 1;
+}
+UT_REGISTER_TEST(test_bloch_irflash_frw_init_relax);
+//FIXME: Test for effect on derivatives
+
 
 static bool test_bloch_ode_obs_irflash(void)
 {
