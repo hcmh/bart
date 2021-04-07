da9d9b72f00f92e5c1dfa62b9fcfdd6bc09b71e2
Nick Scholand <nick.scholand@med.uni-goettingen.de>
2021-04-07 18:16:23 +0200

    Add test for Mxy sampling for IR FLASH Bloch nlop

--- utests/test_bloch_op.c
+++ utests/test_bloch_op.c
@@ -34,7 +34,115 @@
 
 #include "utest.h"
 
-static bool test_bloch_irflash_frw_der(void)
+
+static bool test_bloch_irflash_frw_der_xy(void)
+{
+	enum { N = 16 };
+	enum { rep = 300 };
+	long map_dims[N] = { 16, 16, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long out_dims[N] = { 16, 16, 1, 1, 1, rep, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long in_dims[N] = { 16, 16, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long all_dims[N] = { 16, 16, 1, 1, 1, rep, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	long TI_dims[N] = { 1, 1, 1, 1, 1, rep, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+	bool gpu_use = false;
+
+	// Init and apply Bloch model operator
+
+	complex float* src = md_alloc(N, in_dims, CFL_SIZE);
+	md_zfill(N, in_dims, src, 1.0);
+
+	complex float* dst_frw_bloch = md_alloc(N, out_dims, CFL_SIZE);
+	complex float* dst_der_bloch = md_alloc(N, out_dims, CFL_SIZE);
+
+	struct modBlochFit fit_para = modBlochFit_defaults;
+
+	// IR FLASH characteristics
+	fit_para.sequence = 5;
+	fit_para.tr = 0.003;
+	fit_para.te = 0.001;
+	fit_para.fa = 8.;
+	fit_para.rfduration = 0.00001;
+	fit_para.inversion_pulse_length = 0.;
+	fit_para.prep_pulse_length = 0.;
+	fit_para.look_locker_assumptions = false;
+
+	struct nlop_s* Bloch = nlop_Bloch_create(N, all_dims, map_dims, out_dims, in_dims, &fit_para, gpu_use);
+
+	nlop_apply(Bloch, N, out_dims, dst_frw_bloch, N, in_dims, src);
+	nlop_derivative(Bloch, N, out_dims, dst_der_bloch, N, in_dims, src);
+
+	// dump_cfl("_dst_frw_bloch", N, out_dims, dst_frw_bloch);
+	// dump_cfl("_dst_der_bloch", N, out_dims, dst_der_bloch);
+
+	nlop_free(Bloch);
+	md_free(src);
+
+
+	// Init and apply IR FLASH model operator
+
+	in_dims[COEFF_DIM] = 2;
+
+	complex float* src2 = md_alloc(N, in_dims, CFL_SIZE);
+	md_zfill(N, in_dims, src2, 1.0);
+
+	complex float* dst_frw_irflash = md_alloc(N, out_dims, CFL_SIZE);
+	complex float* dst_der_irflash = md_alloc(N, out_dims, CFL_SIZE);
+
+	// Inversion times
+	complex float* TI = md_alloc(N, TI_dims, CFL_SIZE);
+
+	for (int i = 0; i < rep; i++)
+		TI[i] =  i * fit_para.tr;
+
+	// alpha map
+	complex float* fa = md_alloc(N, map_dims, CFL_SIZE);
+	md_zfill(N, map_dims, fa, fit_para.fa);
+
+	complex float* alpha = md_alloc(N, map_dims, CFL_SIZE);
+
+	fa_to_alpha(DIMS, map_dims, alpha, fa, fit_para.tr);
+
+	md_free(fa);
+
+	struct nlop_s* T1 = nlop_T1_alpha_in_create(N, map_dims, out_dims, in_dims, TI_dims, TI, alpha, gpu_use);
+
+	nlop_apply(T1, N, out_dims, dst_frw_irflash, N, in_dims, src2);
+	nlop_derivative(T1, N, out_dims, dst_der_irflash, N, in_dims, src2);
+
+	// dump_cfl("_dst_frw_irflash", N, out_dims, dst_frw_irflash);
+	// dump_cfl("_dst_der_irflash", N, out_dims, dst_der_irflash);
+
+	nlop_free(T1);
+	md_free(src2);
+	md_free(TI);
+	md_free(alpha);
+
+	// Compare operator outputs
+
+	float err_frw = md_znrmse(N, out_dims, dst_frw_irflash, dst_frw_bloch);
+	// debug_printf(DP_INFO, "Error Forward: %f\n", err_frw);
+
+	float err_der = md_znrmse(N, out_dims, dst_der_irflash, dst_der_bloch);
+	// debug_printf(DP_INFO, "Error Derivative: %f\n", err_der);
+
+
+	md_free(dst_frw_bloch);
+	md_free(dst_der_bloch);
+	md_free(dst_frw_irflash);
+	md_free(dst_der_irflash);
+
+	if (err_frw > 3.E-3)
+		return 0;
+
+	if (err_der > 5.E-3)
+		return 0;
+
+	return 1;
+}
+UT_REGISTER_TEST(test_bloch_irflash_frw_der_xy);
+
+static bool test_bloch_irflash_frw_der_z(void)
 {
 	enum { N = 16 };
 	enum { rep = 300 };
@@ -140,9 +248,9 @@ static bool test_bloch_irflash_frw_der(void)
 
 	return 1;
 }
-UT_REGISTER_TEST(test_bloch_irflash_frw_der);
+UT_REGISTER_TEST(test_bloch_irflash_frw_der_z);
 
-static bool test_bloch_irflash_adj(void)
+static bool test_bloch_irflash_adj_z(void)
 {
 	enum { N = 16 };
 	enum { rep = 300 };
@@ -255,10 +363,10 @@ static bool test_bloch_irflash_adj(void)
 
 	return 1;
 }
-UT_REGISTER_TEST(test_bloch_irflash_adj);
+UT_REGISTER_TEST(test_bloch_irflash_adj_z);
 
 
-static bool test_bloch_irflash_frw_der_spoke_av(void)
+static bool test_bloch_irflash_frw_der_spoke_av_z(void)
 {
 	enum { N = 16 };
 	enum { rep = 300 };
@@ -365,10 +473,10 @@ static bool test_bloch_irflash_frw_der_spoke_av(void)
 
 	return 1;
 }
-UT_REGISTER_TEST(test_bloch_irflash_frw_der_spoke_av);
+UT_REGISTER_TEST(test_bloch_irflash_frw_der_spoke_av_z);
 
 
-static bool test_bloch_irflash_adj_spoke_av(void)
+static bool test_bloch_irflash_adj_spoke_av_z(void)
 {
 	enum { N = 16 };
 	enum { rep = 300 };
@@ -483,9 +591,9 @@ static bool test_bloch_irflash_adj_spoke_av(void)
 
 	return 1;
 }
-UT_REGISTER_TEST(test_bloch_irflash_adj_spoke_av);
+UT_REGISTER_TEST(test_bloch_irflash_adj_spoke_av_z);
 
-static bool test_bloch_irflash_frw_init_relax(void)
+static bool test_bloch_irflash_frw_init_relax_z(void)
 {
 	enum { N = 16 };
 	enum { rep = 300 };
@@ -599,7 +707,7 @@ static bool test_bloch_irflash_frw_init_relax(void)
 
 	return 1;
 }
-UT_REGISTER_TEST(test_bloch_irflash_frw_init_relax);
+UT_REGISTER_TEST(test_bloch_irflash_frw_init_relax_z);
 //FIXME: Test for effect on derivatives
 
 
