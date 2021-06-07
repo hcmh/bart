27fae54521d0a8b66997eab669b830990109a7ca
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2021-06-07 15:18:36 +0200

    missing comma

--- src/grecon/optreg.h
+++ src/grecon/optreg.h
@@ -18,7 +18,7 @@ struct linop_s;
 
 struct reg_s {
 
-	enum { L1WAV, NIHTWAV, NIHTIM, TV, LLR, MLR, IMAGL1, IMAGL2, L1IMG, L2IMG, FTL1, LAPLACE, POS, TENFL L2MAN, L1MAN } xform;
+	enum { L1WAV, NIHTWAV, NIHTIM, TV, LLR, MLR, IMAGL1, IMAGL2, L1IMG, L2IMG, FTL1, LAPLACE, POS, TENFL, L2MAN, L1MAN } xform;
 
 	unsigned int xflags;
 	unsigned int jflags;
