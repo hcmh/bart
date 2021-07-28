aa47d996f5628c9f46be78cca9ac1bd1d3f18f0d
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-07-28 22:40:26 +0200

    add zsqrt

--- src/nlops/someops.c
+++ src/nlops/someops.c
@@ -531,6 +531,72 @@ const struct nlop_s* nlop_zmax_create(int N, const long dims[N], unsigned long f
 }
 
 
+struct zsqrt_s {
+
+	INTERFACE(nlop_data_t);
+
+	int N;
+	const long* dims;
+	complex float* xn;
+};
+
+DEF_TYPEID(zsqrt_s);
+
+static void zsqrt_fun(const nlop_data_t* _data, complex float* dst, const complex float* src)
+{
+	const auto data = CAST_DOWN(zsqrt_s, _data);
+
+	if (NULL == data->xn)
+		data->xn = md_alloc_sameplace(data->N, data->dims, CFL_SIZE, dst);
+
+	md_zsqrt(data->N, data->dims, dst, src);
+	md_zfill(data->N, data->dims, data->xn, 0.5);
+	md_zdiv(data->N, data->dims, data->xn, data->xn, dst);
+}
+
+static void zsqrt_der(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+{
+	UNUSED(o);
+	UNUSED(i);
+
+	const auto data = CAST_DOWN(zsqrt_s, _data);
+	md_zmul(data->N, data->dims, dst, src, data->xn);
+}
+
+static void zsqrt_adj(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+{
+	UNUSED(o);
+	UNUSED(i);
+
+	const auto data = CAST_DOWN(zsqrt_s, _data);
+	md_zmulc(data->N, data->dims, dst, src, data->xn);
+}
+
+static void zsqrt_del(const nlop_data_t* _data)
+{
+	const auto data = CAST_DOWN(zsqrt_s, _data);
+
+	md_free(data->xn);
+	xfree(data->dims);
+	xfree(data);
+}
+
+const struct nlop_s* nlop_zsqrt_create(int N, const long dims[N])
+{
+	PTR_ALLOC(struct zsqrt_s, data);
+	SET_TYPEID(zsqrt_s, data);
+
+	PTR_ALLOC(long[N], ndims);
+	md_copy_dims(N, *ndims, dims);
+
+	data->N = N;
+	data->dims = *PTR_PASS(ndims);
+	data->xn = NULL;
+
+	return nlop_create(N, dims, N, dims, CAST_UP(PTR_PASS(data)),
+		zsqrt_fun, zsqrt_der, zsqrt_adj, NULL, NULL, zsqrt_del);
+}
+
 struct zrss_s {
 
 	INTERFACE(nlop_data_t);
