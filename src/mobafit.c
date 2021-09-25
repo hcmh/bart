b5da883b8314c1f7dcca88b665996fd5c5705f1a
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-09-25 18:55:51 +0200

    speedup mobafit with basis support

--- src/mobafit.c
+++ src/mobafit.c
@@ -7,6 +7,7 @@
  * 2020-2021 Zhengguo Tan <zhengguo.tan@med.uni-goettingen.de>
  */
 
+#include <assert.h>
 #include <stdbool.h>
 #include <complex.h>
 
@@ -28,6 +29,10 @@
 #include "iter/lsqr.h"
 
 #include "nlops/nlop.h"
+#include "nlops/chain.h"
+#include "nlops/cast.h"
+#include "linops/fmac.h"
+#include "nlops/nlop_jacobian.h"
 
 #include "moba/meco.h"
 #include "moba/T1fun.h"
@@ -69,6 +74,7 @@ int main_mobafit(int argc, char* argv[])
 	unsigned int iter = 5;
 
 	const char* init_file = NULL;
+	const char* basis_file = NULL;
 
 	const struct opt_s opts[] = {
 
@@ -85,23 +91,58 @@ int main_mobafit(int argc, char* argv[])
 		OPT_VEC3('p', &patch_size, "x:y:z", "(patch size) [default: 1:1:1]"),
 		OPT_INFILE('I', &init_file, "init", "File for initialization"),
 		OPT_SET('g', &use_gpu, "use gpu"),
+		OPT_INFILE('B', &basis_file, "file", "temporal (or other) basis"),
 	};
 
 	cmdline(&argc, argv, ARRAY_SIZE(args), args, help_str, ARRAY_SIZE(opts), opts);
 
 	num_init();
 
+	long bas_dims[DIMS];
+	complex float* basis = NULL;
+
+	if (NULL != basis_file) {
+
+		basis = load_cfl(basis_file, DIMS, bas_dims);
+		md_zconj(DIMS, bas_dims, basis, basis);
+	}
 
 	long TE_dims[DIMS];
 	complex float* TE = load_cfl(TE_file, DIMS, TE_dims);
 
-	long y_dims[DIMS];
-	complex float* y = load_cfl(echo_file, DIMS, y_dims);
+	long y1_dims[DIMS];
+	long y2_dims[DIMS];
+	complex float* y = load_cfl(echo_file, DIMS, y1_dims);
+	md_copy_dims(DIMS, y2_dims, y1_dims);
 
-	assert(y_dims[TE_DIM] == TE_dims[TE_DIM]);
+	if (NULL == basis) {
+
+		assert(y1_dims[TE_DIM] == TE_dims[TE_DIM]);
+	} else {
+
+		assert(bas_dims[TE_DIM] == TE_dims[TE_DIM]);
+
+		if (1 != y1_dims[TE_DIM]) {
+
+			assert(y1_dims[TE_DIM] == TE_dims[TE_DIM]);
+			y2_dims[COEFF_DIM] = bas_dims[COEFF_DIM];
+			y2_dims[TE_DIM] = 1;
+			complex float* ny = anon_cfl(NULL, DIMS, y2_dims);
+
+			md_ztenmul(DIMS, y2_dims, ny, bas_dims, basis, y1_dims, y);
+
+			unmap_cfl(DIMS, y1_dims, y);
+
+			y = ny;
+		} else {
+
+			y1_dims[TE_DIM] = TE_dims[TE_DIM];
+			y1_dims[COEFF_DIM] = 1;
+		}
+	}
 
 	long x_dims[DIMS];
-	md_select_dims(DIMS, ~TE_FLAG, x_dims, y_dims);
+	md_select_dims(DIMS, ~(TE_FLAG | COEFF_FLAG), x_dims, y1_dims);
 	x_dims[COEFF_DIM] = (IR_LL == seq) ? 3 : set_num_of_coeff(mgre_model);
 
 
@@ -128,9 +169,13 @@ int main_mobafit(int argc, char* argv[])
 	}
 
 
-	long y_patch_dims[DIMS];
-	md_select_dims(DIMS, TE_FLAG, y_patch_dims, y_dims);
-	md_copy_dims(3, y_patch_dims, patch_size);
+	long y1_patch_dims[DIMS];
+	md_copy_dims(DIMS, y1_patch_dims, y1_dims);
+	md_copy_dims(3, y1_patch_dims, patch_size);
+
+	long y2_patch_dims[DIMS];
+	md_copy_dims(DIMS, y2_patch_dims, y2_dims);
+	md_copy_dims(3, y2_patch_dims, patch_size);
 
 	long x_patch_dims[DIMS];
 	md_select_dims(DIMS, COEFF_FLAG, x_patch_dims, x_dims);
@@ -148,24 +193,40 @@ int main_mobafit(int argc, char* argv[])
 		complex float* xref = ptr[2];
 
 		// create signal model
-		struct nlop_s* nlop = NULL;
+		const struct nlop_s* nlop = NULL;
 
 		switch (seq) {
 
 		case IR_LL:  ;
 
-			nlop = nlop_T1_create(DIMS, map_patch_dims, y_patch_dims, x_patch_dims, y_patch_dims, TE, use_gpu);
+			nlop = nlop_T1_create(DIMS, map_patch_dims, y1_patch_dims, x_patch_dims, y1_patch_dims, TE, use_gpu);
 			break;
 
 		case MGRE:  ;
 
 			float scale_fB0[2] = { 0., 1. };
-			nlop = nlop_meco_create(DIMS, y_patch_dims, x_patch_dims, TE, mgre_model, false, FAT_SPEC_1, scale_fB0, use_gpu);
+			nlop = nlop_meco_create(DIMS, y1_patch_dims, x_patch_dims, TE, mgre_model, false, FAT_SPEC_1, scale_fB0, use_gpu);
 			break;
 
 		default:
 
 			error("sequence type not supported");
+		}
+
+		if (NULL != basis) {
+
+			long max_dims[DIMS];
+			md_max_dims(DIMS, ~0, max_dims, y2_patch_dims, bas_dims);
+			nlop = nlop_chain_FF(nlop, nlop_from_linop_F(linop_fmac_create(DIMS, max_dims, TE_FLAG, COEFF_FLAG, ~(TE_FLAG | COEFF_FLAG), basis)));
+
+			long tdims[DIMS];
+			md_copy_dims(DIMS, tdims, y2_patch_dims);
+			tdims[TE_DIM] = tdims[COEFF_DIM];
+			tdims[COEFF_DIM] = 1;
+
+			//nlop = nlop_reshape_out_F(nlop, 0, DIMS, tdims);
+			//nlop = nlop_zprecomp_jacobian_F(nlop);
+			//nlop = nlop_reshape_out_F(nlop, 0, DIMS, y2_patch_dims);
 		}
 
 		struct iter_conjgrad_conf conjgrad_conf = iter_conjgrad_defaults;
@@ -178,25 +239,25 @@ int main_mobafit(int argc, char* argv[])
 		irgnm_conf.iter = iter;
 
 
-		complex float* y_patch    = md_alloc(DIMS, y_patch_dims, CFL_SIZE);
+		complex float* y_patch    = md_alloc(DIMS, y2_patch_dims, CFL_SIZE);
 		complex float* x_patch    = md_alloc(DIMS, x_patch_dims, CFL_SIZE);
 		complex float* xref_patch = md_alloc(DIMS, x_patch_dims, CFL_SIZE);
 
 		long pos[DIMS] = { 0 };
 
-		md_copy_block(DIMS, pos, y_patch_dims,    y_patch, y_dims, y   , CFL_SIZE);
+		md_copy_block(DIMS, pos, y2_patch_dims,    y_patch, y2_dims, y   , CFL_SIZE);
 		md_copy_block(DIMS, pos, x_patch_dims,    x_patch, x_dims, x   , CFL_SIZE);
 		md_copy_block(DIMS, pos, x_patch_dims, xref_patch, x_dims, xref, CFL_SIZE);
 
-		if (0. == md_znorm(DIMS, y_patch_dims, y_patch)) {
+		if (0. == md_znorm(DIMS, y2_patch_dims, y_patch)) {
 
 			debug_printf(DP_WARN, "source images are zero!\n");
 			md_zfill(DIMS, x_patch_dims, x_patch, 0.);
 		} else {
 
-			iter4_irgnm2(CAST_UP(&irgnm_conf), nlop,
+			iter4_irgnm2(CAST_UP(&irgnm_conf), (struct nlop_s*)nlop,
 					2 * md_calc_size(DIMS, x_patch_dims), (float*)x_patch, (float*)xref_patch,
-					2 * md_calc_size(DIMS, y_patch_dims), (const float*)y_patch, lsqr,
+					2 * md_calc_size(DIMS, y2_patch_dims), (const float*)y_patch, lsqr,
 					(struct iter_op_s){ NULL, NULL });
 
 			md_copy_block(DIMS, pos, x_dims, x, x_patch_dims, x_patch, CFL_SIZE);
@@ -214,14 +275,14 @@ int main_mobafit(int argc, char* argv[])
 	long x_strs[DIMS];
 	long y_strs[DIMS];
 	md_calc_strides(DIMS, x_strs, x_dims, CFL_SIZE);
-	md_calc_strides(DIMS, y_strs, y_dims, CFL_SIZE);
+	md_calc_strides(DIMS, y_strs, y2_dims, CFL_SIZE);
 
 	long loop_dims[DIMS];
 	md_singleton_dims(DIMS, loop_dims);
 	for (int i = 0; i < 3; i++) {
 
-		assert(0 == y_dims[i] % patch_size[i]);
-		loop_dims[i] = y_dims[i] / patch_size[i];
+		assert(0 == y2_dims[i] % patch_size[i]);
+		loop_dims[i] = y2_dims[i] / patch_size[i];
 
 		x_strs[i] *= patch_size[i];
 		y_strs[i] *= patch_size[i];
@@ -231,12 +292,15 @@ int main_mobafit(int argc, char* argv[])
 
 	md_free(xref);
 
-	unmap_cfl(DIMS, y_dims, y);
+	unmap_cfl(DIMS, y2_dims, y);
 	unmap_cfl(DIMS, TE_dims, TE);
 	unmap_cfl(DIMS, x_dims, x);
 
 	if (NULL != init_file)
 		unmap_cfl(DIMS, init_dims, init);
+
+	if (NULL != basis_file)
+		unmap_cfl(DIMS, bas_dims, basis);
 
 	double recosecs = timestamp() - start_time;
 
