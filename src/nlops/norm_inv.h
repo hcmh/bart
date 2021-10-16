159b63f8ac9fd9887486c40d3b93a12d5c464b82
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-10-16 14:47:49 +0200

    Add generic normal inversion for nlops

---
+++ src/nlops/norm_inv.h
@@ -0,0 +1,13 @@
+struct nlop_norm_inv_conf {
+
+	_Bool store_nlop;
+	struct iter_conjgrad_conf* iter_conf;
+};
+
+struct linop_s;
+
+extern struct nlop_norm_inv_conf nlop_norm_inv_default;
+
+extern const struct nlop_s* norm_inv_create(struct nlop_norm_inv_conf* conf, const struct nlop_s* normal_op);
+extern const struct nlop_s* norm_inv_lambda_create(struct nlop_norm_inv_conf* conf, const struct nlop_s* normal_op, unsigned long lflag);
+extern const struct nlop_s* norm_inv_lop_lambda_create(struct nlop_norm_inv_conf* conf, const struct linop_s* lop, unsigned long lflags);
